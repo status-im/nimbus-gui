a98028b61af0ac2d06e02e656c5143e57d40e772
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-13 17:32:41 +0300

    Integrate react router dom
    Install react route dom library and create first route

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import { TamaguiProvider } from "tamagui";
 
 import config from "./tamagui.config.js";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
 const App = () => {
-  return <TamaguiProvider config={config}></TamaguiProvider>;
+  return (
+    <RouterProvider router={router}>
+      <TamaguiProvider config={config} />
+    </RouterProvider>
+  );
 };
 
 export default App;
