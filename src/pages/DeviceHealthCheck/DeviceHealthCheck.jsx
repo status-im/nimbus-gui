c487a6d26f2f005f93ee1365f00c12a0c3d8dc4c
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-07-14 12:58:50 +0300

    Add shadow to main photo

--- src/pages/DeviceHealthCheck/DeviceHealthCheck.jsx
+++ src/pages/DeviceHealthCheck/DeviceHealthCheck.jsx
@@ -1,7 +1,9 @@
 const DeviceHealthCheck = () => {
   return (
     <div>
-      <img src="/background-images/eye-background.png" alt="eye-background" />
+      <p className="vignette">
+        <img src="/background-images/eye-background.png" alt="eye-background" />
+      </p>
     </div>
   );
 };
