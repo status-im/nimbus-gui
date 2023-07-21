f7efdd799e205a3f4a7b220878261125c67fd487
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-21 15:31:30 +0300

    Use custom components and remove css

--- src/pages/DeviceHealthCheck/DeviceHealthCheck.jsx
+++ src/pages/DeviceHealthCheck/DeviceHealthCheck.jsx
@@ -1,10 +1,35 @@
+import { Image, XStack, YStack } from "tamagui";
+import { ReactButton } from "../../components/ReactButton";
+import { InformationBox } from "../../components/InformationBox/InformationBox";
+
 const DeviceHealthCheck = () => {
   return (
-    <div className="box">
-      <div className="vignette">
-        <img src="/background-images/eye-background.png" alt="eye-background" />
-      </div>
-    </div>
+    <XStack
+      space
+      backgroundColor="white"
+      width={"632px"}
+      style={{ display: "flex", justifyContent: "start" }}
+    >
+      <YStack space>
+        <InformationBox
+          icon="/icons/close.png"
+          text="The information provided in the Nodes Health Check is meant to
+            utilized as a guide to guage the readiness of your device, however
+            please do your own due diligence prior to commiting any funds. Read
+            our Health Check Disclosure for more information."
+        />
+        <ReactButton color="#fff" backgroundColor="#2A4AF5" fontSize="$5">
+          Continue
+        </ReactButton>
+      </YStack>
+      <Image
+        source={{
+          uri: "/background-images/eye-background.png",
+          width: "650px",
+          height: "100vh",
+        }}
+      />
+    </XStack>
   );
 };
 
