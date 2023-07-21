a7b51d46b0a0abddeb787f5bf6cae5e77785062b
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-21 23:41:49 +0300

    Use new separated components

--- src/components/InformationBox/InformationBox.jsx
+++ src/components/InformationBox/InformationBox.jsx
@@ -8,17 +8,12 @@ const InformationBox = ({ icon, text }) => {
         border: "2px solid #E7EAEE",
         borderRadius: "12px",
         padding: "11px 16px",
-        width: "632px",
+        maxWidth: "632px",
         alignItems: "start",
       }}
       space={"$2"}
     >
-      <Icon
-        source={icon}
-        width={12}
-        height={12}
-        style={{ marginTop: "6px" }}
-      />
+      <Icon source={icon} width={12} height={12} style={{ marginTop: "6px" }} />
       <Paragraph color={"#647084"}>{text}</Paragraph>
     </XStack>
   );
--- src/pages/DeviceHealthCheck/DeviceHealthCheck.jsx
+++ src/pages/DeviceHealthCheck/DeviceHealthCheck.jsx
@@ -1,116 +1,54 @@
-import { XStack, YStack, styled } from "tamagui";
+import { XStack, YStack } from "tamagui";
 import { ReactButton } from "../../components/ReactButton";
 import { Logo } from "../../components/Logo";
 import { InformationBox } from "../../components/InformationBox";
 import { Titles } from "../../components/Titles";
 import { IconText } from "../../components/IconText/IconText";
-import { StatisticBox } from "./StatisticBox";
 import { BackgroundImage } from "../../components/BackgroundImage";
-
-const Box = styled("div", {
-  display: "flex",
-  justifyContent: "center",
-  backgroundColor: "white",
-  width: "100vw",
-});
+import { PageWrapper } from "../../components/PageWrapper";
+import { StatisticBoxes } from "./StatisticBoxes/StatisticBoxes";
+import { StatisticTexts } from "./StatisticTexts";
 
 const DeviceHealthCheck = () => {
+  const continueHandler = (e) => {
+    console.log(e);
+  };
+
   return (
-    <Box>
-      <XStack
-        space={"$5"}
-        style={{
-          background: "rgb(245,242,254)",
-          background:
-            "linear-gradient(180deg, rgba(245,242,254,1) 0%, rgba(255,255,255,1) 100%)",
-          borderRadius: "25px",
-          border: "4px solid #09101C",
-          width: "100%",
-          display: "flex",
-          justifyContent: "end",
-          alignItems: "center",
-        }}
-      >
-        <YStack space={"$3"}>
-          <Logo />
-          <Titles
-            title={"Device Health Check"}
-            subtitle={"Configure your device to start Staking on Nimbus"}
-          />
-          <XStack space={"$3"}>
-            <StatisticBox
-              style={{
-                border: "1px solid #D92344",
-                backgroundColor: "#F3EAEB",
-              }}
-              title="Storage"
-              memory="30 GB"
-              stateIcon="/icons/warning.png"
-              stateText="Poor"
-              additionalStateText="86% Utilization"
-            />
-            <StatisticBox
-              title="CPU"
-              memory="30 GB"
-              stateIcon="/icons/check-circle.png"
-              stateText="Good"
-            />
-          </XStack>
-          <XStack space={"$3"}>
-            <StatisticBox
-              title="Memory"
-              memory="30 GB"
-              stateIcon="/icons/check-circle.png"
-              stateText="Good"
-            />
-            <StatisticBox
-              title="Network"
-              memory="30 GB"
-              stateIcon="/icons/active.png"
-              stateText="Fair"
-            />
-          </XStack>
-          <YStack space={"$2"}>
-            <IconText
-              icon={"/icons/remove-circle-red.png"}
-              text={
-                "Your storage is running low as required for additional node services."
-              }
-            />
-            <IconText
-              icon={"/icons/checkmark-circle-green.png"}
-              text={"2.4GHz is recommended for CPU."}
-            />
-            <IconText
-              icon={"/icons/checkmark-circle-green.png"}
-              text={"There is sufficient ram required for selected services."}
-            />
-            <IconText
-              icon={"/icons/checkmark-circle-blue.png"}
-              text={"Network Latency is low."}
-            />
-          </YStack>
-          <InformationBox
-            icon="/icons/close.png"
-            text="The information provided in the Nodes Health Check is meant to
+    <PageWrapper>
+      <YStack space={"$3"}>
+        <Logo />
+        <Titles
+          title={"Device Health Check"}
+          subtitle={"Configure your device to start Staking on Nimbus"}
+        />
+        <StatisticBoxes />
+        <StatisticTexts />
+        <InformationBox
+          icon="/icons/close.png"
+          text="The information provided in the Nodes Health Check is meant to
             utilized as a guide to guage the readiness of your device, however
             please do your own due diligence prior to commiting any funds. Read
             our Health Check Disclosure for more information."
-          />
-          <XStack>
-            <ReactButton color="#fff" backgroundColor="#2A4AF5" fontSize="$5">
-              Continue
-            </ReactButton>
-          </XStack>
-        </YStack>
-        <BackgroundImage
-          style={{
-            background:
-              "url(/public/background-images/eye-background.png) no-repeat",
-          }}
         />
-      </XStack>
-    </Box>
+        <XStack>
+          <ReactButton
+            color="#fff"
+            backgroundColor="#2A4AF5"
+            fontSize="$5"
+            onClick={continueHandler}
+          >
+            Continue
+          </ReactButton>
+        </XStack>
+      </YStack>
+      <BackgroundImage
+        style={{
+          background:
+            "url(/public/background-images/eye-background.png) no-repeat",
+        }}
+      />
+    </PageWrapper>
   );
 };
 
