b239a14e62d968d80f0a8eae947ebc889c33c172
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-21 20:19:55 +0300

    Create reusable shadow box component

---
+++ src/components/ShadowBox/ShadowBox.jsx
@@ -0,0 +1,9 @@
+import { styled } from "tamagui";
+
+const ShadowBox = styled("div", {
+  boxSizing: "border-box",
+  borderRadius: "16px",
+  boxShadow: "0px 4px 20px 0px rgba(9, 16, 28, 0.08)",
+});
+
+export { ShadowBox };
