a3a0a015bed2ee74c1b7001300cb71e8ce0c2155
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-21 20:19:16 +0300

    Create background image component

---
+++ src/components/BackgroundImage/BackgroundImage.jsx
@@ -0,0 +1,9 @@
+import { styled } from "tamagui";
+
+const BackgroundImage = styled("div", {
+  boxShadow: "inset 100px 0px 100px white",
+  width: "650px",
+  height: "100vh",
+});
+
+export { BackgroundImage };
