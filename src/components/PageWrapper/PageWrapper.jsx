18443f3139ebd2dbf98b21a929ee2bdae722e499
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-21 23:13:28 +0300

    Create page wrapper

---
+++ src/components/PageWrapper/PageWrapper.jsx
@@ -0,0 +1,33 @@
+import { XStack, styled } from "tamagui";
+
+const Background = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "white",
+  width: "100vw",
+});
+
+const PageWrapper = ({ children, ...props }) => {
+  return (
+    <Background>
+      <XStack
+        space={"$5"}
+        style={{
+          background: "rgb(245,242,254)",
+          background:
+            "linear-gradient(180deg, rgba(245,242,254,1) 0%, rgba(255,255,255,1) 100%)",
+          borderRadius: "25px",
+          border: "4px solid #09101C",
+          width: "100%",
+          display: "flex",
+          justifyContent: "end",
+          alignItems: "center",
+        }}
+      >
+        {children}
+      </XStack>
+    </Background>
+  );
+};
+
+export { PageWrapper };
