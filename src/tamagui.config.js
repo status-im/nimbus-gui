30d84280d1e3e026a5dd4b60fab4b6406ef095d9
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-21 15:29:19 +0300

    Create tamagui config with @tamagui/config

--- src/tamagui.config.js
+++ src/tamagui.config.js
@@ -1,24 +1,4 @@
-import { shorthands } from "@tamagui/shorthands";
-import { themes, tokens } from "@tamagui/themes";
-import { createFont, createTamagui } from "tamagui";
+import { config } from "@tamagui/config";
+import { createTamagui } from "tamagui";
 
-export default createTamagui({
-  themes,
-  tokens,
-  shorthands,
-  fonts: {
-    body: createFont({
-      family: "Arial",
-      size: {
-        // You'll want to fill these values in so you can use them
-        // for now, fontSize="$4" will be 14px.
-        // You can define different keys, but `tamagui`
-        // standardizes on 1-15.
-        4: 14,
-      },
-      lineHeight: {
-        4: 16,
-      },
-    }),
-  },
-});
+export default createTamagui(config);
