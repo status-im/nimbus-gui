a9d9eb7d223ab7b896836c9237ef9e486bd4f538
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-07-22 13:14:27 +0300

    Use icon button into titles

--- src/components/Titles/Titles.jsx
+++ src/components/Titles/Titles.jsx
@@ -1,11 +1,24 @@
-import { YStack } from "tamagui";
+import { XStack, YStack } from "tamagui";
 import { SubTitle } from "./Title";
 import { Title } from "./SubTitle";
+import { IconButton } from "../IconButton";
 
 const Titles = ({ title, subtitle }) => {
   return (
     <YStack>
-      <Title color={"#09101C"}>{title}</Title>
+      <XStack justifyContent="space-between">
+        <Title color={"#09101C"}>{title}</Title>
+        <IconButton
+          style={{
+            backgroundColor: "transparent",
+            border: "1px solid #DCE0E5",
+          }}
+          color={"#09101C"}
+          size={"$3"}
+          icon={"/icons/reveal.png"}
+          text={"Advanced Settings"}
+        />
+      </XStack>
       <SubTitle color={"#09101C"}>{subtitle}</SubTitle>
     </YStack>
   );
