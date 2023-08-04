6e03a12c4da9de551b0e2f08fd97cd7172685ec6
Rickard Andersson <gonz@severnatazvezda.com>
2023-08-04 12:18:37 +0300

    feat: add `@status-im/components` library

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Button, TamaguiProvider } from 'tamagui'
 import './App.css'
-import '@tamagui/react-17-patch'
+import { Button as StatusButton, Provider as StatusProvider } from '@status-im/components'
 
 import config from '../tamagui.config'
 
@@ -12,23 +12,26 @@ function App() {
 
   return (
     <TamaguiProvider config={config}>
-      <Button size="$6">LEL</Button>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <StatusProvider>
+        <Button size="$6">Tamagui LEL</Button>
+        <StatusButton>Status LEL</StatusButton>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      </StatusProvider>
     </TamaguiProvider>
   )
 }
