4cae5c67a19dfd79f55ff2e1e19c36bbd07f00da
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-07 14:20:54 +0300

    refactor: add types and clean FormattedText

---
+++ src/components/FormattedText/FormattedText.tsx
@@ -0,0 +1,36 @@
+import { Text } from 'tamagui'
+
+type TextElement = {
+  text: string
+  bold?: boolean
+  italic?: boolean
+}
+
+type FormattedTextProps = {
+  textElements: TextElement[]
+  color?: string
+  fontSize?: string
+}
+
+const FormattedText = ({ textElements, color, fontSize }: FormattedTextProps) => {
+  const calculateStyle = (textElement: TextElement) => {
+    const isBold = textElement.bold ?? false
+    const isItalic = textElement.italic ?? false
+
+    return { fontWeight: isBold ? 'bold' : '', fontStyle: isItalic ? 'italic' : '' }
+  }
+
+  return (
+    <Text color={color} fontSize={fontSize}>
+      {textElements.map((textElement, index) => {
+        return (
+          <span style={calculateStyle(textElement)} key={index}>
+            {textElement.text}
+          </span>
+        )
+      })}
+    </Text>
+  )
+}
+
+export { FormattedText }
