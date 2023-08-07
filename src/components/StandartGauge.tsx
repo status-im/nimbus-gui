df45d98cfb64ee747629f5f4afdc30cb81bbf84e
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-07 15:27:39 +0300

    Remove react fragments

--- src/components/StandartGauge.tsx
+++ src/components/StandartGauge.tsx
@@ -10,24 +10,19 @@ interface StandartGaugeProps {
   data: Data[]
 }
 
-const StandartGauge = (props: StandartGaugeProps) => {
-  const { data } = props
+const StandartGauge = ({ data }: StandartGaugeProps) => (
+  <ResponsivePie
+    data={data}
+    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+    innerRadius={0.65}
+    colors={datum => datum.data.color}
+    fit={false}
+    activeOuterRadiusOffset={8}
+    enableArcLinkLabels={false}
+    arcLinkLabelsColor={{ from: 'color' }}
+    enableArcLabels={false}
+    legends={[]}
+  />
+)
 
-  return (
-    <>
-      <ResponsivePie
-        data={data}
-        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-        innerRadius={0.65}
-        colors={datum => datum.data.color}
-        fit={false}
-        activeOuterRadiusOffset={8}
-        enableArcLinkLabels={false}
-        arcLinkLabelsColor={{ from: 'color' }}
-        enableArcLabels={false}
-        legends={[]}
-      />
-    </>
-  )
-}
 export default StandartGauge
