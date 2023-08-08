ce712c621b6fcc83480af20845270ca36b8786d5
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-08 09:06:24 +0300

    refactor: convert Icon jsx component to tsx

--- src/components/Icon.jsx
+++
@@ -1,7 +0,0 @@
-import { Image } from 'tamagui'
-
-const Icon = ({ source, width = 16, height = 16, ...props }) => {
-  return <Image {...props} source={{ uri: source }} width={width} height={height} />
-}
-
-export default Icon
---
+++ src/components/Icon.tsx
@@ -0,0 +1,13 @@
+import { Image } from 'tamagui'
+
+type IconProps = {
+  source: string
+  width?: number
+  height?: number
+}
+
+const Icon = ({ source, width = 16, height = 16, ...props }: IconProps) => {
+  return <Image {...props} source={{ uri: source }} width={width} height={height} />
+}
+
+export default Icon
