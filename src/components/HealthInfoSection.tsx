0947232fa163610c968bf13567b47fe6636b2212
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-08 13:57:33 +0300

    feat: add constant messages

--- src/components/HealthInfoSection.tsx
+++ src/components/HealthInfoSection.tsx
@@ -1,5 +1,9 @@
 import { YStack } from 'tamagui'
 
+const BAD_STORAGE_TEXT = 'Your storage is running low as required for additional node services.'
+const CPU_CLOCK_RATE_TEXT = '2.4GHz is recommended for CPU.'
+const GOOD_RAM_MEMORY_TEXT = 'There is sufficient ram required for selected services.'
+const NETWORK_TEXT = 'Network Latency is low.'
 type HealthInfoSectionProps = {
   usedStorage: number
   maxStorage: number
