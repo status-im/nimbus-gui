e0a75a6d253391fe6087e46475900a55322f5e91
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-08 10:10:52 +0300

    refactor: ShadowBox tsx and replace div

--- src/components/ShadowBox.tsx
+++ src/components/ShadowBox.tsx
@@ -1,6 +1,6 @@
-import { styled } from 'tamagui'
+import { Stack, styled } from 'tamagui'
 
-const ShadowBox = styled('div', {
+const ShadowBox = styled(Stack, {
   boxSizing: 'border-box',
   borderRadius: '16px',
   boxShadow: '0px 4px 20px 0px rgba(9, 16, 28, 0.08)',
