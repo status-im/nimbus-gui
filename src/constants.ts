e533617a6b1154546997a68c137a22c62ba179e1
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-09 10:20:39 +0300

    feat: create constants file and move them

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const GREEN_CHECKMARK_ICON = '/icons/checkmark-circle-green.png'
+export const RED_CHECKMARK_ICON = '/icons/remove-circle-red.png'
+
+export const BAD_STORAGE_TEXT =
+  'Your storage is running low as required for additional node services.'
+export const CPU_CLOCK_RATE_TEXT = '2.4GHz is recommended for CPU.'
+export const GOOD_RAM_MEMORY_TEXT = 'There is sufficient RAM required for selected services.'
+export const NETWORK_TEXT = 'Network Latency is low.'
