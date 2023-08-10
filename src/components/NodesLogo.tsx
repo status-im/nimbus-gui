544ddff5292169275ff985b94f7afdf43176f088
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-10 09:25:20 +0300

    feat: create nodes logo

---
+++ src/components/NodesLogo.tsx
@@ -0,0 +1,14 @@
+import { XStack } from 'tamagui'
+
+const NodesLogo = () => {
+  return (
+    <XStack
+      style={{
+        alignItems: 'center',
+      }}
+      space={'$2'}
+    ></XStack>
+  )
+}
+
+export default NodesLogo
