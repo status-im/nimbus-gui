f61f2f2e2ddd3693ce0650b68cd62f0013dbea96
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-10 09:54:37 +0300

    feat: add content to NimbusLogo

--- src/components/NodesLogo.tsx
+++ src/components/NodesLogo.tsx
@@ -1,13 +1,20 @@
-import { XStack } from 'tamagui'
+import { Text, XStack } from 'tamagui'
+import Icon from './Icon'
+import Tag from './Tag'
 
 const NodesLogo = () => {
   return (
     <XStack
       style={{
         alignItems: 'center',
+        justifyContent: 'center',
       }}
       space={'$2'}
-    ></XStack>
+    >
+      <Icon source={'src/assets/nodes-app-icon.png'} width={32} height={32} />
+      <Text style={{ fontWeight: '700', fontSize: '28px' }}>nodes</Text>
+      <Tag bc="#2A4AF5" text="BETA" />
+    </XStack>
   )
 }
 
