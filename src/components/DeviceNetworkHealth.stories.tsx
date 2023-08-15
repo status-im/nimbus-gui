f335f3f2b0a149f3dc61e20466c55b02d6ef7745
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-15 09:12:11 +0300

    feat: add story with empty arr for network

--- src/components/DeviceNetworkHealth.stories.tsx
+++ src/components/DeviceNetworkHealth.stories.tsx
@@ -16,14 +16,21 @@ type Story = StoryObj<typeof meta>
 
 export const GoodStats: Story = {
   args: {
-    uploadRate: [1, 4, 23, 61,34],
-    downloadRate: [20, 3, 40, 56,32],
+    uploadRate: [1, 4, 23, 61, 34],
+    downloadRate: [20, 3, 40, 56, 32],
   },
 }
 
 export const BadStats: Story = {
   args: {
     uploadRate: [1, 4, 23, 55],
-    downloadRate: [20, 3, 40, 56,80],
+    downloadRate: [20, 3, 40, 56, 80],
+  },
+}
+
+export const NoStats: Story = {
+  args: {
+    uploadRate: [],
+    downloadRate: [],
   },
 }
