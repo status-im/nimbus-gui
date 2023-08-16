0555e52bdb72ab9f923d848cae90c874f5ea2376
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-16 13:40:23 +0300

    Implement settings section

--- src/pages/PairDevice/PairDevice.tsx
+++ src/pages/PairDevice/PairDevice.tsx
@@ -2,7 +2,7 @@ import { Separator, XStack, YStack } from 'tamagui'
 import PageWrapperShadow from '../../components/PageWrappers/PageWrapperShadow'
 import SyncStatus from './SyncStatus'
 import NimbusLogo from '../../components/Logos/NimbusLogo'
-import { Button, Tag } from '@status-im/components'
+import { Button, Checkbox, Tag, Text } from '@status-im/components'
 import PairIcon from '../../components/Icons/PairIcon'
 import CreateIcon from '../../components/Icons/CreateIcon'
 import NodeIcon from '../../components/Icons/NodeIcon'
@@ -25,6 +25,22 @@ const PairDevice = () => {
         </XStack>
         <Separator borderColor={'#e3e3e3'} />
         <SyncStatus isPairing={true} timer={'00:12'} isAwaitingPairing={true} />
+        <Separator borderColor={'#e3e3e3'} />
+        <Text size={19} weight={'semibold'} color="#09101C">
+          Settings
+        </Text>
+        <XStack space={'$4'}>
+          <Checkbox
+            id="port-checkbox"
+            variant="outline"
+            selected={false}
+            onCheckedChange={() => {
+              console.log('checked')
+            }}
+            size={20}
+          />
+          <Text size={15}>Auto Connect Paired Device</Text>
+        </XStack>
         <Separator borderColor={'#e3e3e3'} />
         <XStack>
           <Button icon={<NodeIcon />}>Continue</Button>
