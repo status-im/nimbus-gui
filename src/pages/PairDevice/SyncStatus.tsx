a88d7900e38db093bfef3c8deb8badd5ea064f57
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-16 13:27:08 +0300

    fix: remove constant width

--- src/pages/PairDevice/SyncStatus.tsx
+++ src/pages/PairDevice/SyncStatus.tsx
@@ -11,7 +11,7 @@ type SyncStatusProps = {
 }
 const SyncStatus = ({ isPairing, timer, isAwaitingPairing }: SyncStatusProps) => {
   return (
-    <YStack style={{ width: '584px' }}>
+    <YStack>
       <XStack space={'$4'} style={{ justifyContent: 'space-between' }}>
         <Text size={11} color="#647084" weight="medium">
           Device Sync Status
