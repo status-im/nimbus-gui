d25c13396cba6eb4b0049cd94bc77c5c426205b1
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-16 13:21:36 +0300

    feat: add continue section

--- src/pages/PairDevice/PairDevice.tsx
+++ src/pages/PairDevice/PairDevice.tsx
@@ -2,9 +2,10 @@ import { Separator, XStack, YStack } from 'tamagui'
 import PageWrapperShadow from '../../components/PageWrappers/PageWrapperShadow'
 import SyncStatus from './SyncStatus'
 import NimbusLogo from '../../components/Logos/NimbusLogo'
-import { Tag } from '@status-im/components'
+import { Button, Tag } from '@status-im/components'
 import PairIcon from '../../components/Icons/PairIcon'
 import CreateIcon from '../../components/Icons/CreateIcon'
+import NodeIcon from '../../components/Icons/NodeIcon'
 
 const PairDevice = () => {
   return (
@@ -24,6 +25,10 @@ const PairDevice = () => {
         </XStack>
         <Separator borderColor={'#e3e3e3'} />
         <SyncStatus isPairing={true} timer={'00:12'} isAwaitingPairing={true} />
+        <Separator borderColor={'#e3e3e3'} />
+        <XStack>
+          <Button icon={<NodeIcon />}>Continue</Button>
+        </XStack>
       </YStack>
     </PageWrapperShadow>
   )
