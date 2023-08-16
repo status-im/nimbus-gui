2b7f29607eb478d6ecc423c9888baacdc9d20e65
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-16 13:13:43 +0300

    feat: add header with logo and tags

--- src/pages/PairDevice/PairDevice.tsx
+++ src/pages/PairDevice/PairDevice.tsx
@@ -1,6 +1,10 @@
-import { Separator, YStack } from 'tamagui'
+import { Separator, XStack, YStack } from 'tamagui'
 import PageWrapperShadow from '../../components/PageWrappers/PageWrapperShadow'
 import SyncStatus from './SyncStatus'
+import NimbusLogo from '../../components/Logos/NimbusLogo'
+import { Tag } from '@status-im/components'
+import PairIcon from '../../components/Icons/PairIcon'
+import CreateIcon from '../../components/Icons/CreateIcon'
 
 const PairDevice = () => {
   return (
@@ -11,6 +15,13 @@ const PairDevice = () => {
           maxWidth: '100%',
         }}
       >
+        <XStack style={{ justifyContent: 'space-between' }}>
+          <NimbusLogo />
+          <XStack space={'$2'} style={{ alignItems: 'center' }}>
+            <Tag icon={PairIcon} label="Pair" size={32} selected />
+            <Tag icon={CreateIcon} label="Create" size={32} />
+          </XStack>
+        </XStack>
         <Separator borderColor={'#e3e3e3'} />
         <SyncStatus isPairing={true} timer={'00:12'} isAwaitingPairing={true} />
       </YStack>
