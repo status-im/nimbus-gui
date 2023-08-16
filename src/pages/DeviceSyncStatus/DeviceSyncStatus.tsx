51088658950c98bb7b82d6933736b7ae515f9d88
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-16 13:45:33 +0300

    fix: change titles and remove container

--- src/pages/DeviceSyncStatus/DeviceSyncStatus.tsx
+++ src/pages/DeviceSyncStatus/DeviceSyncStatus.tsx
@@ -27,31 +27,28 @@ const DeviceSyncStatus = () => {
           ]}
         />
       </Stack>
-      <div className="container-inner landing-page">
-        <YStack
-          space={'$4'}
-          style={{
-            justifyContent: 'end',
-            alignItems: 'start',
-            marginBottom: '2rem',
-            maxWidth: '100%',
-          }}
-        >
-          <NimbusLogo />
-          <Titles
-            title="Device Health Check"
-            subtitle="Configure your device to start Staking on Nimbus"
-            isAdvancedSettings={true}
-          />
-          <YStack>
-            <SyncStatusCardExecution synced={132432} total={200000} />
-            <SyncStatusCardConsensus synced={149500} total={160000} />
-          </YStack>
-          <Stack style={{ marginTop: '1rem' }}>
-            <Button>Continue</Button>
-          </Stack>
+      <YStack
+        space={'$4'}
+        style={{
+          justifyContent: 'end',
+          alignItems: 'start',
+          marginBottom: '2rem',
+          maxWidth: '100%',
+        }}
+      >
+        <NimbusLogo />
+        <Titles
+          title="Sync Status"
+          subtitle="Monitor your Validator Client and Beacon Node syncing progression."
+        />
+        <YStack>
+          <SyncStatusCardExecution synced={132432} total={200000} />
+          <SyncStatusCardConsensus synced={149500} total={160000} />
         </YStack>
-      </div>
+        <Stack style={{ marginTop: '1rem' }}>
+          <Button>Continue</Button>
+        </Stack>
+      </YStack>
     </PageWrapperShadow>
   )
 }
