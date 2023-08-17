156f1774978b0d06bc83ffd03f1c74aea93f391a
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-17 17:59:17 +0300

    feat: add story for successfully paired

---
+++ src/pages/PairDevice/PairedSuccessfully.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import PairedSuccessfully from './PairedSuccessfully'
+
+const meta = {
+  title: 'Pair Device/PairedSuccessfully',
+  component: PairedSuccessfully,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof PairedSuccessfully>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const PairedSuccessfullyExample: Story = {
+  args: {},
+}
