86771e6ab7c17b444958d3827b0bb4ddde1cb829
Ivana Andersson <ivana.p.kostova@gmail.com>
2023-08-21 17:22:46 +0300

    fix: change margins on landing page to vh

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,11 @@
-import PageWrapperShadow from '../../components/PageWrappers/PageWrapperShadow'
 import './LandingPage.css'
-import QuickStartBar from '../../components/General/QuickStartBar/QuickStartBar'
-// import Titles from '../../components/General/Titles'
 import { XStack, YStack } from 'tamagui'
+import PageWrapperShadow from '../../components/PageWrappers/PageWrapperShadow'
 import Title from '../../components/General/Title'
 import NimbusLogo from '../../components/Logos/NimbusLogo'
 import NodeIcon from '../../components/Icons/NodeIcon'
 import { Button as StatusButton, Text } from '@status-im/components'
+import QuickStartBar from '../../components/General/QuickStartBar/QuickStartBar'
 
 function LandingPage() {
   return (
@@ -17,10 +16,8 @@ function LandingPage() {
             <NimbusLogo />
           </XStack>
 
-          <YStack style={{ width: '100%', margin: '7em 0 4em' }}>
-            <Title color={'#09101C'}>
-              Light and performant clients, for all Ethereum validators.
-            </Title>
+          <YStack style={{ width: '100%', margin: '30vh 0 4vh' }}>
+            <Title>Light and performant clients, for all Ethereum validators.</Title>
             <Text size={15} weight="regular">
               <strong>Nimbus Nodes</strong> allows you to take control and ownership of the services
               you wish to run in a completely trustless and decentralized manner.
