7d1c6913d8d4b059f502214dc12f26a4313fe798
Rickard Andersson <gonz@severnatazvezda.com>
2023-08-21 15:57:29 +0300

    fix(ci): remove `viteFinal` in config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,14 +16,6 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag',
   },
-
-  async viteFinal(config, { configType }) {
-    if (configType === 'PRODUCTION') {
-      config.base = './'
-    }
-
-    return config
-  },
 }
 
 export default config
