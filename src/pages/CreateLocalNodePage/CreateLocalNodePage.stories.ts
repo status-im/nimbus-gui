91cae3f39db7ed7e9f6dce2f36fa4911eae6ca08
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-22 13:17:20 +0300

    fix: add decorators with router

--- src/pages/CreateLocalNodePage/CreateLocalNodePage.stories.ts
+++ src/pages/CreateLocalNodePage/CreateLocalNodePage.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import CreateLocalNodePage from './CreateLocalNodePage'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 const meta = {
   title: 'Pages/CreateLocalNodePage',
@@ -10,6 +11,7 @@ const meta = {
   },
   tags: ['autodocs'],
   argTypes: {},
+  decorators: [withRouter],
 } satisfies Meta<typeof CreateLocalNodePage>
 
 export default meta
