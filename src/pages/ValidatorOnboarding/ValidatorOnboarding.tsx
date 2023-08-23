036232b7ed9404525f478f0cb9777c21b6487341
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-23 12:22:18 +0300

    Add shadow box

--- src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
+++ src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
@@ -2,10 +2,11 @@ import { XStack, YStack } from 'tamagui'
 import PageWrapperShadow from '../../components/PageWrappers/PageWrapperShadow'
 import Title from '../../components/General/Title'
 import { NodeIcon } from '@status-im/icons'
-import { Button as StatusButton, Text } from '@status-im/components'
+import { Shadow, Button as StatusButton, Text } from '@status-im/components'
 import QuickStartBar from '../../components/General/QuickStartBar/QuickStartBar'
 import { useNavigate } from 'react-router'
 import FormStepper from './Stepper'
+import Titles from '../../components/General/Titles'
 
 function ValidatorOnboarding() {
   const navigate = useNavigate()
@@ -16,8 +17,8 @@ function ValidatorOnboarding() {
 
   return (
     <>
-      <PageWrapperShadow rightImageSrc="./background-images/landing-page-bg.png">
-        <YStack className="landing-page">
+      <PageWrapperShadow>
+        <YStack>
           <YStack style={{ width: '100%', margin: '30vh 0 4vh' }}>
             <Text size={19} weight={'semibold'}>
               Create Nimbus Validator
@@ -27,15 +28,25 @@ function ValidatorOnboarding() {
             </Text>
           </YStack>
           <FormStepper />
-
-          <XStack>
-            <StatusButton icon={<NodeIcon size={20} />} onPress={getStartedHanlder}>
-              Get Started
-            </StatusButton>
-          </XStack>
+          <Shadow
+            variant="$2"
+            style={{
+              width: '1052px',
+              height: '523px',
+              borderRadius: '16px',
+              border: 'none',
+              backgroundColor: '#fff',
+            }}
+          >
+            <YStack>
+              <Titles
+                title="Overview"
+                subtitle="Becoming a validator is a big responsibility with important preparation steps. Only start the deposit process when youre ready."
+              ></Titles>
+            </YStack>
+          </Shadow>
         </YStack>
       </PageWrapperShadow>
-      <QuickStartBar />
     </>
   )
 }
