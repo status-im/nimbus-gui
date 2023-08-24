228d7a2932a0c7b72fde65c05e9da5545782ba7b
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-24 11:53:01 +0300

    feat: create component for key generation page

---
+++ src/pages/ValidatorOnboarding/KeyGeneration.tsx
@@ -0,0 +1,23 @@
+import { Button, Shadow } from '@status-im/components'
+import { Stack } from 'tamagui'
+
+const KeyGeneration = () => {
+  return (
+    <>
+      <Shadow
+        variant="$1"
+        style={{
+          borderRadius: '16px',
+          border: 'none',
+          backgroundColor: '#fff',
+          padding: '16px 32px',
+        }}
+      ></Shadow>
+      <Stack style={{ alignItems: 'end', width: '100%', marginTop: '16px' }}>
+        <Button>Continue</Button>
+      </Stack>
+    </>
+  )
+}
+
+export default KeyGeneration
--- src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
+++ src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
@@ -4,6 +4,7 @@ import PageWrapperGradient from '../../components/PageWrappers/PageWrapperGradie
 import Titles from '../../components/General/Titles'
 import { useState } from 'react'
 import Overview from './Overview'
+import KeyGeneration from './KeyGeneration'
 
 const ValidatorOnboarding = () => {
   const [activeStep, setActiveStep] = useState(0)
@@ -30,6 +31,7 @@ const ValidatorOnboarding = () => {
         />
         <FormStepper activeStep={activeStep} changeActiveStep={changeActiveStep} />
         {activeStep === 0 && <Overview />}
+        {activeStep === 3 && <KeyGeneration />}
       </YStack>
     </PageWrapperGradient>
   )
