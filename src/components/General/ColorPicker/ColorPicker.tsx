edb5e3a64b125b1c3e5fdc59ac060b6148e78df9
Ivana Andersson <ivana.p.kostova@gmail.com>
2023-08-24 13:04:38 +0300

    feat: create custom color picker component

--- src/components/General/ColorPicker/ColorPicker.tsx
+++ src/components/General/ColorPicker/ColorPicker.tsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import { CirclePicker } from 'react-color'
 import { XStack } from 'tamagui'
+import { CustomPicker } from 'react-color'
+import './ColorPicker.css'
 
 const ColorPicker = () => {
-  const [chosenColor, setChosenColor] = useState('#FFFFFF')
+  const [chosenColor, setChosenColor] = useState('#2A4AF5')
+  const colors = [
+    '#2A4AF5',
+    '#F6B03C',
+    '#7140FD',
+    '#2A799B',
+    '#EC266C',
+    '#1992D7',
+    '#FF7D46',
+    '#216266',
+    '#F66F8F',
+    '#C78F67',
+    '#CB6256',
+  ]
+
   return (
-    <XStack my={10}>
-      <CirclePicker
-        width="80%"
-        circleSize={40}
-        circleSpacing={12}
-        colors={[
-          '#2A4AF5',
-          '#F6B03C',
-          '#7140FD',
-          '#2A799B',
-          '#EC266C',
-          '#1992D7',
-          '#FF7D46',
-          '#216266',
-          '#F66F8F',
-          '#C78F67',
-          '#CB6256',
-        ]}
-        color={chosenColor}
-        onChange={color => setChosenColor(color.hex)}
-      />
-    </XStack>
+    <>
+      <XStack my={10} width="80%" flexWrap="wrap">
+        {colors.map((color, i) => (
+          <button
+            key={i}
+            type="button"
+            className="color-picker-button"
+            onClick={_e => setChosenColor(color)}
+            style={{ background: color, color: color }}
+          ></button>
+        ))}
+      </XStack>
+    </>
   )
 }
 
-export default ColorPicker
+export default CustomPicker(ColorPicker)
