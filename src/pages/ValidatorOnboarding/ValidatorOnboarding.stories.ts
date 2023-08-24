b62916825054297b31e5568db483b901cf9f1682
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-24 11:21:19 +0300

    feat: create story for validator page

---
+++ src/pages/ValidatorOnboarding/ValidatorOnboarding.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+import ValidatorOnboarding from './ValidatorOnboarding'
+
+const meta = {
+  title: 'Pages/ValidatorOnboarding',
+  component: ValidatorOnboarding,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [withRouter],
+} satisfies Meta<typeof ValidatorOnboarding>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Page: Story = {
+  args: {},
+}
