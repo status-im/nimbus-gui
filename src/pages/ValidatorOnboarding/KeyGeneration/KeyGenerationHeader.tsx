1ce402491d93ec5bed1a21f799f702772c1f8bf2
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-25 12:24:43 +0300

    feat: add cards and styling

--- src/pages/ValidatorOnboarding/KeyGeneration/KeyGenerationHeader.tsx
+++ src/pages/ValidatorOnboarding/KeyGeneration/KeyGenerationHeader.tsx
@@ -1,16 +1,16 @@
 import { Text } from '@status-im/components'
 import { XStack } from 'tamagui'
-// import SyncCard from './SyncCard'
+import SyncCard from './SyncCard'
 
 const KeyGenerationHeader = () => {
   return (
-    <XStack justifyContent="space-between">
+    <XStack style={{ width: '100%', alignItems: 'center', justifyContent: 'space-between' }}>
       <Text size={27} weight={'semibold'}>
         Key Generation
       </Text>
       <XStack space={'$2'}>
-        {/* <SyncCard synced={123.524} total={172.503} />
-        <SyncCard synced={123.524} total={172.503} /> */}
+        <SyncCard synced={123.524} total={172.503} title="Execution Sync Status" />
+        <SyncCard synced={123.524} total={172.503} title="Consensus Sync Status" />
       </XStack>
     </XStack>
   )
