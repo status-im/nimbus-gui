260f23c714961d7003ba0c165e67a029b023099e
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-25 10:25:26 +0300

    feat: add logic to continue for the last step

--- src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
+++ src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
@@ -10,12 +10,22 @@ import Activation from './Activation/Activation'
 import './layoutGradient.css'
 import ValidatorBoxWrapper from './ValidatorBoxWrapper/ValidatorBoxWrapper'
 import { Button } from '@status-im/components'
+import { useNavigate } from 'react-router-dom'
 
 const ValidatorOnboarding = () => {
   const [activeStep, setActiveStep] = useState(0)
+  const navigate = useNavigate()
 
   const changeActiveStep = (step: number) => {
     setActiveStep(step)
+  }
+
+  const continueHandler = () => {
+    if (activeStep < 4) {
+      setActiveStep(activeStep + 1)
+    } else {
+      navigate('/')
+    }
   }
 
   return (
@@ -43,7 +53,9 @@ const ValidatorOnboarding = () => {
           {activeStep === 4 && <Activation />}
         </ValidatorBoxWrapper>
         <Stack style={{ alignItems: 'end', width: '100%', marginTop: '16px', zIndex: 999 }}>
-          <Button>Continue</Button>
+          <Button onPress={continueHandler}>
+            {activeStep < 4 ? 'Continue' : 'Continue to Dashboard'}
+          </Button>
         </Stack>
       </YStack>
     </div>
