7eb1307a44a8b74d802a61bfcb16eb49802eef5e
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-25 11:42:55 +0300

    Add made up texts

--- src/pages/ValidatorOnboarding/Advicsories/Advicsories.tsx
+++ src/pages/ValidatorOnboarding/Advicsories/Advicsories.tsx
@@ -10,18 +10,37 @@ type AdvisoryTopicsType = {
 const unicodeNumbers = ['➀', '➁', '➂', '➃', '➄', '➅']
 
 const advisoryTopics: AdvisoryTopicsType = {
-  'Proof of Stake': ['This is content for Proof of Stake...'],
-  Deposit: ['This is content for Deposit...'],
-  'Key Management': ['This is content for Key Management...'],
+  'Proof of Stake': [
+    'Proof of Stake systems require validators to hold and lock up a certain amount of cryptocurrency to participate.',
+    'In Proof of Stake, the chances of creating a block is proportional to the amount of cryptocurrency held.',
+    'Unlike Proof of Work, Proof of Stake aims to achieve consensus without intensive computational work.'
+  ],
+  Deposit: [
+    'Deposits are often irreversible, so ensure to double-check transaction details before confirming.',
+    'Delay in deposit acknowledgment might be due to network congestion or node synchronization.',
+    'Always keep transaction IDs or hashes for records and future references in case of disputes.'
+  ],
+  'Key Management': [
+    'Storing your private keys on a device connected to the internet is susceptible to hacks and malware.',
+    'Hardware wallets provide an added layer of security by keeping private keys isolated from online systems.',
+    'Regularly back up and encrypt your key management solutions to prevent potential losses.'
+  ],
   'Bad Behaviour': [
     'If you try to cheat the system, or act contrary to the specification, you will be liable to incur a penalty known as slashing.',
     'Running your validator keys simultaneously on two or more machines will result in slashing.',
-    'Simply being offline with an otherwise healthy network does not result in slashing, but will result in small inactivity penalties.',
+    'Simply being offline with an otherwise healthy network does not result in slashing, but will result in small inactivity penalties.'
   ],
-  Requirements: ['This is content for Requirements...'],
-  Risks: ['This is content for Risks...'],
+  Requirements: [
+    'Ensure your system meets the minimum software and hardware requirements before initiating any operations.',
+    'Staying updated with the latest versions is vital to maintain system integrity and performance.',
+    'Failure to meet requirements might result in operational inefficiencies or security vulnerabilities.'
+  ],
+  Risks: [
+    'Cryptocurrency investments are subject to high volatility and can result in both significant gains and losses.',
+    'Always do thorough research before making investment decisions or engaging in transactions.',
+    'Be wary of phishing scams, malicious software, and too-good-to-be-true offers.'
+  ],
 }
-
 const Advicsories = () => {
   const [selectedTitle, setSelectedTitle] = useState('Bad Behaviour')
 
