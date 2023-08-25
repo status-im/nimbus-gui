269ad8393adc8d06f92cdc69ddd37a3f7c949680
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-25 14:24:07 +0300

    Add logic and style

--- src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorSetup.tsx
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorSetup.tsx
@@ -1,9 +1,32 @@
-import { YStack } from 'tamagui'
+import { XStack, Stack, Text as TextTam, YStack } from 'tamagui'
 import PairedDeviceCard from './PairedDeviceCard'
-import { XStack } from 'tamagui'
 import { Text } from '@status-im/components'
+import ExecClientCard from './ExecClientCard'
 
 const ValidatorSetup = () => {
+  const execClientCardsContent = [
+    {
+      name: 'Nethermind',
+      icon: '/icons/nethermind-circle.png',
+    },
+    {
+      name: 'Besu',
+      icon: '/icons/open-ethereum-circle',
+    },
+    {
+      name: 'Geth',
+      icon: '/icons/teku-circle',
+    },
+    {
+      name: 'Erigon',
+      icon: '/icons/lighthouse-circle',
+    },
+    {
+      name: 'Nimbus',
+      icon: '/icons/prysm-circle',
+      isComingSoon: true,
+    },
+  ]
   return (
     <YStack style={{ width: '100%' }}>
       <XStack justifyContent={'space-between'}>
@@ -11,6 +34,31 @@ const ValidatorSetup = () => {
           Validator Setup
         </Text>
         <PairedDeviceCard />
+      </XStack>
+
+      <YStack>
+        <Stack style={{ marginBottom: '4px' }}>
+          <Text size={13} color="#647084">
+            Execution Client Detection
+          </Text>
+        </Stack>
+        <Text size={15} weight={'regular'}>
+          No existing execution client installations have been detected on paired device.
+        </Text>
+        <Text size={13} color="#828282">
+          If you believe this to be incorrect please test your pairing to the correct device and try
+          again.
+        </Text>
+      </YStack>
+
+      <TextTam fontSize={27} style={{ margin: '5px', marginLeft: 0 }}>
+        Select Execution client
+      </TextTam>
+      <XStack justifyContent={'space-between'}>
+        {execClientCardsContent.length &&
+          execClientCardsContent.map(e => (
+            <ExecClientCard name={e.name} icon={e.icon} isComingSoon={e.isComingSoon || false} />
+          ))}
       </XStack>
     </YStack>
   )
