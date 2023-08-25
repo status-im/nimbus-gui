6108b69ca923a30da335d8c7119fe0e16b32088e
Ivana Andersson <ivana.p.kostova@gmail.com>
2023-08-25 11:32:01 +0300

    feat: add the emoji picker to the create avatar component

--- src/components/General/CreateAvatar/CreateAvatar.tsx
+++ src/components/General/CreateAvatar/CreateAvatar.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import { XStack, YStack } from 'tamagui'
-import LabelInputField from '../LabelInputField'
 import { Avatar, Text } from '@status-im/components'
-import ColorPicker from '../ColorPicker/ColorPicker'
 import { ReactionIcon } from '@status-im/icons'
+import LabelInputField from '../LabelInputField'
+import ColorPicker from '../ColorPicker/ColorPicker'
+import EmojiPickerDialog from '../EmojiPickerDialog'
 
 const CreateAvatar = () => {
+  const [isEmojiDialogOpen, setIsEmojiDialogOpen] = useState(false)
   return (
     <YStack my={16}>
       <XStack space>
@@ -15,9 +18,20 @@ const CreateAvatar = () => {
           <Text size={13} weight="regular" color={'#647084'}>
             Device Avatar
           </Text>
-          <XStack my={10}>
-            <Avatar type="account" size={80} name="Device Avatar" />
-            <ReactionIcon size={20} />
+          <XStack my={10} alignItems={'end'}>
+            <Avatar type="user" size={80} name="Device Avatar" />
+            <Avatar
+              type="icon"
+              size={32}
+              icon={
+                <ReactionIcon
+                  size={20}
+                  onClick={() => setIsEmojiDialogOpen(prev => !prev)}
+                  style={{ cursor: 'pointer' }}
+                />
+              }
+            />
+            {isEmojiDialogOpen && <EmojiPickerDialog emojiStyle="TWITTER" />}
           </XStack>
         </YStack>
         <YStack>
