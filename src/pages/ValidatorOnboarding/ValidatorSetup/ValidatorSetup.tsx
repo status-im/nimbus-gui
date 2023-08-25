e76bf01361158c7a5d38e8d5ddf9bf171b6ba7b0
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-25 12:59:42 +0300

    Add header of page

--- src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorSetup.tsx
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorSetup.tsx
@@ -1,10 +1,17 @@
 import { YStack } from 'tamagui'
 import PairedDeviceCard from './PairedDeviceCard'
+import { XStack } from 'tamagui'
+import { Text } from '@status-im/components'
 
 const ValidatorSetup = () => {
   return (
-    <YStack>
-      <PairedDeviceCard />
+    <YStack style={{ width: '100%' }}>
+      <XStack justifyContent={'space-between'}>
+        <Text size={27} weight={'semibold'}>
+          Validator Setup
+        </Text>
+        <PairedDeviceCard />
+      </XStack>
     </YStack>
   )
 }
