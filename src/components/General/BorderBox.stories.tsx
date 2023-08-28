10ba1e0cf646d0a66b3f77755fb082eaba9ca281
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-28 10:46:16 +0300

    feat: create story for border box component

---
+++ src/components/General/BorderBox.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import BorderBox from './BorderBox'
+
+const meta = {
+  title: 'General/BorderBox',
+  component: BorderBox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof BorderBox>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultColors: Story = {
+  args: {
+    children: 'BorderBox',
+  },
+}
