8445cc06e1dfbd271585f7f973cd7542d5c587fc
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-28 11:57:01 +0300

    feat: add static components to key generation

--- src/pages/ValidatorOnboarding/KeyGeneration/KeyGeneration.tsx
+++ src/pages/ValidatorOnboarding/KeyGeneration/KeyGeneration.tsx
@@ -1,12 +1,29 @@
 import { YStack } from 'tamagui'
 import KeyGenerationHeader from './KeyGenerationHeader'
 import RecoveryMechanism from './RecoveryMechanism'
+import { Button, InformationBox, Text } from '@status-im/components'
+import { CloseCircleIcon } from '@status-im/icons'
 
 const KeyGeneration = () => {
   return (
-    <YStack space={'$2'} style={{ width: '100%', padding: '16px 32px' }}>
+    <YStack space={'$2'} style={{ width: '100%', padding: '16px 32px', alignItems: 'start' }}>
       <KeyGenerationHeader />
       <RecoveryMechanism />
+      <Text size={27} weight={'semibold'}>
+        4 Validators
+      </Text>
+      <Button>Generate Key files</Button>
+      <InformationBox
+        message="You should see that you have one keystore per validator. This keystore contains your signing key, encrypted with your password. Warning: Do not store keys on multiple (backup) validator clients at once"
+        variant="error"
+        icon={<CloseCircleIcon size={20} />}
+      />
+      <Button>Reveal Recovery Phrase</Button>
+      <InformationBox
+        message="Write down and keep your Secret Recovery Phrase in a secure place. Make sure no one is looking at your screen."
+        variant="error"
+        icon={<CloseCircleIcon size={20} />}
+      />
     </YStack>
   )
 }
