64818f12520aeba197f5502b648548c750138a7d
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-29 12:06:54 +0300

    feat: create grid with inputs for confirming

--- src/pages/ValidatorOnboarding/KeyGeneration/ConfirmRecoveryPhrase.tsx
+++ src/pages/ValidatorOnboarding/KeyGeneration/ConfirmRecoveryPhrase.tsx
@@ -1,12 +1,17 @@
-import { YStack } from 'tamagui'
+import { Stack, YStack } from 'tamagui'
 import KeyGenerationTitle from './KeyGenerationTitle'
-import { Text } from '@status-im/components'
+import { Input, Text } from '@status-im/components'
 
 const ConfirmRecoveryPhrase = () => {
   return (
     <YStack space={'$4'} style={{ width: '100%', marginTop: '20px' }}>
       <KeyGenerationTitle />
       <Text size={27}>Confirm Recovery Phrase</Text>
+      <Stack style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '16px 16px' }}>
+        {Array.from({ length: 24 }, (_, index) => (
+          <Input key={`input-${index}`} />
+        ))}
+      </Stack>
     </YStack>
   )
 }
