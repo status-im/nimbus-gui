209b2c706da214dab18a87f5413bca14fda53fa6
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-29 11:54:27 +0300

    Use Activation Card

--- src/pages/ValidatorOnboarding/Activation/Activation.tsx
+++ src/pages/ValidatorOnboarding/Activation/Activation.tsx
@@ -1,33 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { XStack, Stack, YStack } from 'tamagui';
-import { Text } from '@status-im/components';
-import Confetti from 'react-confetti';
+import React, { useState, useEffect } from 'react'
+import { XStack, Stack, YStack } from 'tamagui'
+import { Text } from '@status-im/components'
+import Confetti from 'react-confetti'
+import ActivationCard from './ActivationCard'
 
 const Activation = () => {
-  const [showConfetti, setShowConfetti] = useState(true);
+  const [showConfetti, setShowConfetti] = useState(true)
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setShowConfetti(false);
-    }, 5000);  // Hide after 5 seconds
+      setShowConfetti(false)
+    }, 5000)  
 
     return () => {
-      clearTimeout(timer);  // Clear the timer if the component is unmounted
-    };
-  }, []);  // The empty dependency array means this effect will only run once, similar to `componentDidMount`
+      clearTimeout(timer)  
+    }
+  }, [])  
 
   return (
     <Stack>
-      {showConfetti && <Confetti width={1000} height={800} />}
-      <YStack style={{ width: '100%', padding: '16px 32px' }}>
-        <XStack justifyContent={'space-between'} style={{ marginBottom: '10px' }}>
+      {showConfetti && <Confetti width={1000} height={800} style={{ zIndex: 0 }} />}
+      <YStack style={{ padding: '16px 32px' }}>
+        <YStack space={'$5'}>
           <Text size={27} weight={'semibold'}>
             Validator Setup
           </Text>
-        </XStack>
+          <Text size={19}>
+            Congratulations! You have successfully setup your Nimbus Validators and are currently
+            syncing your nodes.{' '}
+          </Text>
+          <YStack space={'$2'} marginTop={'10px'} width={'33%'}>
+            <XStack space={'$2'} justifyContent={'space-between'}>
+              <ActivationCard text="Validators" value="4" />
+              <ActivationCard text="Execution Sync Status" value="fasfasf af sa" />
+              <ActivationCard text="Execution Sync Status" value="fasfasf af sa" />
+            </XStack>
+            <XStack space={'$2'}>
+              <ActivationCard text="Current APR" value="4.40%" />
+              <ActivationCard text="Estimated Activation Time" value="32 Days" />
+              <ActivationCard text="Estimated Activation Time" value="fasfasf af sa" />
+            </XStack>
+          </YStack>
+        </YStack>
       </YStack>
     </Stack>
   )
 }
 
-export default Activation;
+export default Activation
