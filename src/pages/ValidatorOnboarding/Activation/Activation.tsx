242b068cf411958dafc52622f173a4f5356bf838
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-08-29 11:16:59 +0300

    Create confeti effect

--- src/pages/ValidatorOnboarding/Activation/Activation.tsx
+++ src/pages/ValidatorOnboarding/Activation/Activation.tsx
@@ -1,7 +1,20 @@
-import { YStack } from 'tamagui'
+import { XStack, Stack, YStack } from 'tamagui'
+import { Text } from '@status-im/components'
+import Confetti from 'react-confetti'
 
 const Activation = () => {
-  return <YStack style={{ padding: '16px 32px' }}></YStack>
+  return (
+    <Stack>
+      <Confetti width={1000} height={800} />
+      <YStack style={{ width: '100%', padding: '16px 32px' }}>
+        <XStack justifyContent={'space-between'} style={{ marginBottom: '10px' }}>
+          <Text size={27} weight={'semibold'}>
+            Validator Setup
+          </Text>
+        </XStack>
+      </YStack>
+    </Stack>
+  )
 }
 
 export default Activation
