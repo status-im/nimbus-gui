43492a3dad49a408084aaff60ddc9097aa2820f9
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-29 13:55:29 +0300

    feat: add text and style to reveal phrase

--- src/pages/ValidatorOnboarding/KeyGeneration/RecoveryPhrase.tsx
+++ src/pages/ValidatorOnboarding/KeyGeneration/RecoveryPhrase.tsx
@@ -1,4 +1,4 @@
-import { Stack, YStack } from 'tamagui'
+import { Stack, XStack, YStack } from 'tamagui'
 import { Button, InformationBox, Text } from '@status-im/components'
 import { CloseCircleIcon } from '@status-im/icons'
 import { useState } from 'react'
@@ -20,16 +20,30 @@ const RecoveryPhrase = ({ isKeyFiles }: RecoveryPhraseProps) => {
         style={{
           border: `1px solid #2A4AF566`,
           borderRadius: '16px',
-          padding: '12px 16px',
+          padding: '28px 18px',
           backgroundColor: '#f4f6fe',
           width: '100%',
+          height: '176px',
         }}
       >
-        <Stack style={{ filter: `blur(${isReveal ? '0px' : '4px'})` }}>
-          <Text size={15} weight={'semibold'}>
-            asdf
-          </Text>
-        </Stack>
+        <YStack space={'$2'} style={{ filter: `blur(${isReveal ? '0px' : '4px'})` }}>
+          <XStack space={'$6'}>
+            <Text size={19} weight={'semibold'}>
+              this is your secret recovery phrase for the validator
+            </Text>
+            <Text size={19} weight={'semibold'}>
+              this is your secret recovery phrase for the validator
+            </Text>
+          </XStack>
+          <XStack space={'$6'}>
+            <Text size={19} weight={'semibold'}>
+              this is your secret recovery phrase for the validator
+            </Text>
+            <Text size={19} weight={'semibold'}>
+              this is your secret recovery phrase for the validator
+            </Text>
+          </XStack>
+        </YStack>
       </Stack>
       <Stack style={{ width: 'fit-content', marginBottom: '12px' }}>
         <Button onPress={revealHandler}>Reveal Recovery Phrase</Button>
