092d70ea472de64160f99e2e9b666742924d816a
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-29 11:08:44 +0300

    feat: create flow for confirming recovery phrase

--- src/pages/ValidatorOnboarding/KeyGeneration/KeyGeneration.tsx
+++ src/pages/ValidatorOnboarding/KeyGeneration/KeyGeneration.tsx
@@ -7,33 +7,43 @@ import RecoveryMechanism from './RecoveryMechanism'
 import KeyFiles from './KeyFiles'
 import RecoveryPhrase from './RecoveryPhrase'
 import { BOTH_KEY_AND_RECOVERY, KEY_FILES, RECOVERY_PHRASE } from '../../../constants'
+import ConfirmRecoveryPhrase from './ConfirmRecoveryPhrase'
 
-const KeyGeneration = () => {
+type KeyGenerationProps = {
+  isConfirmPhraseStage: boolean
+}
+
+const KeyGeneration = ({ isConfirmPhraseStage }: KeyGenerationProps) => {
   const [recoveryMechanism, setRecoveryMechanism] = useState(KEY_FILES)
-
-  const handleRecMechanismChange = (value: string) => {
-    setRecoveryMechanism(value)
-  }
 
   const isKeyFiles = recoveryMechanism === KEY_FILES || recoveryMechanism === BOTH_KEY_AND_RECOVERY
 
   const isRecoveryPhrase =
     recoveryMechanism === RECOVERY_PHRASE || recoveryMechanism === BOTH_KEY_AND_RECOVERY
 
+  const handleRecMechanismChange = (value: string) => {
+    setRecoveryMechanism(value)
+  }
+
   return (
     <YStack space={'$2'} style={{ width: '100%', padding: '16px 32px', alignItems: 'start' }}>
-      <KeyGenerationHeader />
-      <RecoveryMechanism
-        recoveryMechanism={recoveryMechanism}
-        handleRecMechanismChange={handleRecMechanismChange}
-      />
-      <Stack style={{ margin: '30px 0' }}>
-        <Text size={27} weight={'semibold'}>
-          4 Validators
-        </Text>
-      </Stack>
-      {isKeyFiles && <KeyFiles />}
-      {isRecoveryPhrase && <RecoveryPhrase isKeyFiles={isKeyFiles} />}
+      {isConfirmPhraseStage && <ConfirmRecoveryPhrase />}
+      {isConfirmPhraseStage === false && (
+        <>
+          <KeyGenerationHeader />
+          <RecoveryMechanism
+            recoveryMechanism={recoveryMechanism}
+            handleRecMechanismChange={handleRecMechanismChange}
+          />
+          <Stack style={{ margin: '30px 0' }}>
+            <Text size={27} weight={'semibold'}>
+              4 Validators
+            </Text>
+          </Stack>
+          {isKeyFiles && <KeyFiles />}
+          {isRecoveryPhrase && <RecoveryPhrase isKeyFiles={isKeyFiles} />}
+        </>
+      )}
     </YStack>
   )
 }
