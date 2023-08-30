0d0412615b08187e3e07349552480f678cdf8b1e
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-30 13:21:34 +0300

    feat: create story for client setup

---
+++ src/pages/ValidatorOnboarding/ClientSetup/ClientSetup.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+import ClientSetup from './ClientSetup'
+
+const meta = {
+  title: 'ValidatorOnboarding/ClientSetup',
+  component: ClientSetup,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [withRouter],
+} satisfies Meta<typeof ClientSetup>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Page: Story = {
+  args: {},
+}
