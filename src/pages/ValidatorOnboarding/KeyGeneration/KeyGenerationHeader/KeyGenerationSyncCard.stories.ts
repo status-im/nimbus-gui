99b150ef6a79a23a1750530c840b7606e3443271
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-30 12:39:02 +0300

    feat: add examples for key generation sync card story

--- src/pages/ValidatorOnboarding/KeyGeneration/KeyGenerationHeader/KeyGenerationSyncCard.stories.ts
+++ src/pages/ValidatorOnboarding/KeyGeneration/KeyGenerationHeader/KeyGenerationSyncCard.stories.ts
@@ -15,11 +15,65 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Page: Story = {
+export const Blue: Story = {
   args: {
     synced: 123.524,
     total: 172.503,
     title: 'Execution Sync Status',
+    color: '#2a4af5',
+  },
+}
+
+export const Red: Story = {
+  args: {
+    synced: 123.524,
+    total: 172.503,
+    title: 'Execution Sync Status',
+    color: '#ff6161',
+  },
+}
+
+export const MaxValue: Story = {
+  args: {
+    synced: 172.503,
+    total: 172.503,
+    title: 'Execution Sync Status',
+    color: '#2a4af5',
+  },
+}
+
+export const OverMaxValue: Story = {
+  args: {
+    synced: 200,
+    total: 172.503,
+    title: 'Execution Sync Status',
+    color: '#2a4af5',
+  },
+}
+
+export const MinValue: Story = {
+  args: {
+    synced: 0,
+    total: 172.503,
+    title: 'Execution Sync Status',
+    color: '#2a4af5',
+  },
+}
+
+export const UnderMinValue: Story = {
+  args: {
+    synced: -100,
+    total: 172.503,
+    title: 'Execution Sync Status',
+    color: '#2a4af5',
+  },
+}
+
+export const WithoutTitle: Story = {
+  args: {
+    synced: 123.524,
+    total: 172.503,
+    title: '',
     color: '#2a4af5',
   },
 }
