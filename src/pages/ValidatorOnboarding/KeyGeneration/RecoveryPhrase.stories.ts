1867d90f389fa64a209bfbde74d1ce9bd2a45c38
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-08-31 06:37:15 +0300

    feat: create story for recovery phrase

---
+++ src/pages/ValidatorOnboarding/KeyGeneration/RecoveryPhrase.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import RecoveryPhrase from './RecoveryPhrase'
+
+const meta = {
+  title: 'ValidatorOnboarding/RecoveryPhrase',
+  component: RecoveryPhrase,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof RecoveryPhrase>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    isKeystoreFiles: false,
+  },
+}
