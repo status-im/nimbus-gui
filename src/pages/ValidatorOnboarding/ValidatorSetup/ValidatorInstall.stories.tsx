b8f5493238d6ad5e66b8fe898012b1e73cb3451f
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-01 10:05:42 +0300

    fix: add redux provider for validator install story

--- src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstall.stories.tsx
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstall.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { Provider as ReduxProvider } from 'react-redux'
 
 import ValidatorSetupInstall from './ValidatorInstall'
+import store from '../../../redux/store'
 
 const meta = {
   title: 'ValidatorOnboarding/ValidatorSetupInstall',
@@ -9,6 +11,13 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  decorators: [
+    StoryObj => (
+      <ReduxProvider store={store}>
+        <StoryObj />
+      </ReduxProvider>
+    ),
+  ],
 } satisfies Meta<typeof ValidatorSetupInstall>
 
 export default meta
