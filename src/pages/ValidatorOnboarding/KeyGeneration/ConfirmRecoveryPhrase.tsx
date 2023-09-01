e8f55ba18dddf23d8feed3fc0f309d60a46009b0
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-01 14:04:20 +0300

    feat: render 24 autocomplete inputs

--- src/pages/ValidatorOnboarding/KeyGeneration/ConfirmRecoveryPhrase.tsx
+++ src/pages/ValidatorOnboarding/KeyGeneration/ConfirmRecoveryPhrase.tsx
@@ -1,7 +1,11 @@
+import AutocompleteInput from './AutocompleteInput'
 
 const ConfirmRecoveryPhrase = () => {
   return (
     <div>
+      {Array.from({ length: 24 }).map((_, index) => (
+        <AutocompleteInput key={index} index={index} />
+      ))}
     </div>
   )
 }
