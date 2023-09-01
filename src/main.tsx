d97967423c4cfb3064a03a99dbe3d9d275c2186b
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-01 14:41:19 +0300

    fix: import and decorator

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider as ReduxProvider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
-import store from './redux/store.tsx'
+import store from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/ValidatorOnboarding/ValidatorOnboarding.stories.ts
+++ src/pages/ValidatorOnboarding/ValidatorOnboarding.stories.ts
@@ -10,7 +10,7 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
-  decorators: [withRouter],
+  decorators: [withRouter()],
 } satisfies Meta<typeof ValidatorOnboarding>
 
 export default meta
