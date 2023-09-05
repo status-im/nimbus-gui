404761447dd64cd75b533fab5d523658994a4da5
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-05 12:15:37 +0300

    feat: create story for autocomplete

---
+++ src/pages/ValidatorOnboarding/KeyGeneration/ConfirmRecoveryPhrase/AutocompleteInput.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import AutocompleteInput from './AutocompleteInput'
+
+const meta = {
+  title: 'ValidatorOnboarding/AutocompleteInput',
+  component: AutocompleteInput,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof AutocompleteInput>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    index: 0,
+  },
+}
