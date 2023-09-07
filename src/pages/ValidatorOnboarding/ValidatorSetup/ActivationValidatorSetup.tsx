53987a8ba4f639dc919fc9c1bbc21ab3f1d5f915
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-07 12:55:16 +0300

    Create ActivationValidatorSetup.tsx

---
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ActivationValidatorSetup.tsx
@@ -0,0 +1,4 @@
+                            Congratulations! You have successfully setup your Execution and Consensus clients and are currently syncing your nodes.  You need to be sufficiently synced prior to setting up your validators and making a deposit.
+                            <ActivationCard text="Current APR" value="4.40%" />
+                            <ActivationCard text="Estimated Activation Time" value="32 Days" />
+                            <ActivationCard text="Validator Queue" value="92603" />
