a198711b246a735aa92b401356d8311a966b1227
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-07 15:50:52 +0300

    feat: create story for continue button

---
+++ src/pages/ValidatorOnboarding/ContinueButton.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+import ContinueButton from './ContinueButton'
+
+const meta = {
+  title: 'ValidatorOnboarding/ContinueButton',
+  component: ContinueButton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [withRouter],
+} satisfies Meta<typeof ContinueButton>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    continueHandler: () => {},
+    activeStep: 0,
+    isConfirmPhraseStage: false,
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    continueHandler: () => {},
+    activeStep: 0,
+    isConfirmPhraseStage: true,
+  },
+}
