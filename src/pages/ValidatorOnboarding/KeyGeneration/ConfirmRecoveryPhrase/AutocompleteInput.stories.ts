0610dea4150ce87e6d7659e02a60cbc458c29faa
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-07 19:38:47 +0300

    fix: autocomplete story

--- src/pages/ValidatorOnboarding/KeyGeneration/ConfirmRecoveryPhrase/AutocompleteInput.stories.ts
+++ src/pages/ValidatorOnboarding/KeyGeneration/ConfirmRecoveryPhrase/AutocompleteInput.stories.ts
@@ -17,23 +17,11 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     index: 0,
-    isValid: true,
-    changeIsValid: () => {},
-  },
-}
-
-export const NotValid: Story = {
-  args: {
-    index: 0,
-    isValid: false,
-    changeIsValid: () => {},
   },
 }
 
 export const OtherWord: Story = {
   args: {
     index: 2,
-    isValid: false,
-    changeIsValid: () => {},
   },
 }
