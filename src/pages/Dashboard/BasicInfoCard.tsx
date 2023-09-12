c98ee6c040005ef14b7f1c6a65c799cd20aea8d5
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-12 22:01:32 +0300

    feat: style and get data for basic card

--- src/pages/Dashboard/BasicInfoCard.tsx
+++ src/pages/Dashboard/BasicInfoCard.tsx
@@ -1,7 +1,30 @@
-import { Stack } from 'tamagui'
+import { Text } from '@status-im/components'
+import { YStack } from 'tamagui'
+import DashboardCardWrapper from './DashboardCardWrapper'
 
-const BasicInfoCard = () => {
-  return <Stack></Stack>
+type BasicInfoCardProps = {
+  title: string
+  value: string
+}
+
+const BasicInfoCard = ({ title, value }: BasicInfoCardProps) => {
+  return (
+    <DashboardCardWrapper>
+      <YStack
+        style={{
+          padding: '12px 16px',
+          width: '100%',
+        }}
+      >
+        <Text size={15} weight={'semibold'}>
+          {title}
+        </Text>
+        <Text size={27} weight={'semibold'}>
+          {value}
+        </Text>
+      </YStack>
+    </DashboardCardWrapper>
+  )
 }
 
 export default BasicInfoCard
