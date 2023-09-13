bf1217c1999395d4c37f4b877c8be978063d0716
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-13 12:06:44 +0300

    feat: first row with cards structure

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,19 @@
-import { YStack } from 'tamagui'
+import { XStack, YStack } from 'tamagui'
 
 import BasicInfoCards from './BasicInfoCards/BasicInfoCards'
 import AddCardsContainer from '../../components/General/AddCards/AddCardsContainer'
+import SyncStatusCard from './SyncStatusCard'
 
 const Dashboard = () => {
   return (
     <YStack space={'$2'} alignItems="start">
-      <AddCardsContainer />
+      <XStack space={'$2'}>
+        <XStack space={'$4'}>
+          <SyncStatusCard />
+          <AddCardsContainer />
+        </XStack>
+        <XStack space={'$2'}></XStack>
+      </XStack>
       <BasicInfoCards />
     </YStack>
   )
