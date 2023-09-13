49c789b834b5f2f6ec97d2cb17e55583c4eab12e
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-13 19:40:18 +0300

    Create BalanceChardCard.stories.tsx

---
+++ src/pages/Dashboard/BalanceLineChart/BalanceChardCard.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import BalanceChardCard from './BalanceChardCard'
+
+const meta = {
+  title: 'Pages/BalanceChardCard',
+  component: BalanceChardCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof BalanceChardCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Page: Story = {
+  args: {},
+}
