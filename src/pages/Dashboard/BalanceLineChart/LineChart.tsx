a663b7c0246332b0a30ad8d5521303bf8b7d7654
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-13 19:39:13 +0300

    Style chart and add figma texts

--- src/pages/Dashboard/BalanceLineChart/LineChart.tsx
+++ src/pages/Dashboard/BalanceLineChart/LineChart.tsx
@@ -22,14 +22,22 @@ ChartJS.register(
 
 
 
+const years = ['APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'JAN', 'FEB', 'MAR', 'APR']
+
+const userGains = [10000, 15000, 17500, 20000, 19000, 23222, 25000, 20000, 20000, 21000, 22300, 21000, 25000]
 
 const data = {
+    type: 'line',
     labels: years,
     datasets: [
         {
 
             data: userGains,
+            borderColor: '#1992D7',
+            backgroundColor: '#f6fbfd',
             fill: true,
+            tension: 0.4,
+            pointRadius: 0,
 
         }
     ],
@@ -39,6 +47,7 @@ const data = {
 
 const options = {
     responsive: true,
+    maintainAspectRatio: true,
 };
 
 const LineChart = () => {
