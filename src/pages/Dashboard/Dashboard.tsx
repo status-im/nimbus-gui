1ca3e1c227ca00f757772d813d19b00240cbc5de
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-13 09:52:16 +0300

    feat: add data and grid for basic cards

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,25 @@
 import { XStack } from 'tamagui'
+import BasicInfoCard from './BasicInfoCard'
 
 const Dashboard = () => {
-  return <XStack></XStack>
+  return (
+    <XStack>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(5, 1fr)',
+          gap: '20px',
+          width: '100%',
+        }}
+      >
+        <BasicInfoCard title="Active Validators" value="6" />
+        <BasicInfoCard title="Connected Peers" value="16" />
+        <BasicInfoCard title="At Headslot" value="-6" />
+        <BasicInfoCard title="Avg. Effectiveness" value="4" />
+        <BasicInfoCard title="Participation Rate" value="6" />
+      </div>
+    </XStack>
+  )
 }
 
 export default Dashboard
