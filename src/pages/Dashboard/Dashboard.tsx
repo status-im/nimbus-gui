6835236ee5f47cc6aac7b6058639875ba36b0f9f
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-13 11:12:13 +0300

    feat: move add cards into container

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,21 +1,12 @@
 import { YStack } from 'tamagui'
 
-import AddCard from '../../components/General/AddCard'
 import BasicInfoCards from './BasicInfoCards/BasicInfoCards'
+import AddCardsContainer from './AddCardsContainer'
 
 const Dashboard = () => {
   return (
-    <YStack>
-      <YStack
-        style={{
-          borderRadius: '16px',
-          boxShadow: '0 0 8px rgba(0, 0, 0, 0.1)',
-          width: 'fit-content',
-        }}
-      >
-        <AddCard />
-        <AddCard />
-      </YStack>
+    <YStack space={'$2'}>
+      <AddCardsContainer />
       <BasicInfoCards />
     </YStack>
   )
