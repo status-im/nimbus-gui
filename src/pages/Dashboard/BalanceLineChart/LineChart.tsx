54330817ae4f7f9fc62eac2a6104d63e3e1785c8
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-15 10:05:23 +0300

    Pass line chart data from parent

--- src/pages/Dashboard/BalanceLineChart/LineChart.tsx
+++ src/pages/Dashboard/BalanceLineChart/LineChart.tsx
@@ -16,32 +16,33 @@ ChartJS.register(
     Filler,
 );
 
-const years = ['APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC', 'JAN', 'FEB', 'MAR', 'APR']
 
-const userGains = [10000, 15000, 17500, 20000, 19000, 23222, 25000, 20000, 20000, 21000, 22300, 21000, 25000]
 
-const data = {
-    type: 'line',
-    labels: years,
-    datasets: [
-        {
-            data: userGains,
-            borderColor: '#1992D7',
-            backgroundColor: '#f6fbfd',
-            fill: true,
-            tension: 0.4,
-            pointRadius: 0,
-
-        }
-    ],
-    options: {
-        responsive: true,
-        maintainAspectRatio: true,
-    }
-
+type LineChartProps = {
+    years: string[];
+    userGains: number[];
 };
+const LineChart = ({ years, userGains }: LineChartProps) => {
+    const data = {
+        type: 'line',
+        labels: years,
+        datasets: [
+            {
+                data: userGains,
+                borderColor: '#1992D7',
+                backgroundColor: '#f6fbfd',
+                fill: true,
+                tension: 0.4,
+                pointRadius: 0,
 
-const LineChart = () => {
+            }
+        ],
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+        }
+
+    };
     return <Line options={data.options} data={data} />;
 }
 
