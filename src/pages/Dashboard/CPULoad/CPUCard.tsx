d84b24ea50b9ba5ad7d9548f6eb0a8a380130fed
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-15 20:44:05 +0300

    fix: format dashboard code

--- src/pages/Dashboard/CPULoad/CPUCard.tsx
+++ src/pages/Dashboard/CPULoad/CPUCard.tsx
@@ -1,88 +1,96 @@
-import StandartLineChart from '../../../components/Charts/StandardLineChart'
-import IconText from '../../../components/General/IconText'
 import { Paragraph, Separator, XStack, YStack } from 'tamagui'
 import { Shadow, Text } from '@status-im/components'
 import { IncorrectIcon } from '@status-im/icons'
+
+import StandartLineChart from '../../../components/Charts/StandardLineChart'
+import IconText from '../../../components/General/IconText'
 import Icon from '../../../components/General/Icon'
 
 type DataPoint = {
-    x: number
-    y: number
+  x: number
+  y: number
 }
 
 type ChartData = {
-    id: string
-    color: string
-    data: DataPoint[]
+  id: string
+  color: string
+  data: DataPoint[]
 }
 
 type CPUCardProps = {
-    load: number[]
+  load: number[]
 }
+
 const CPUCard = ({ load }: CPUCardProps) => {
-    const chartData: ChartData[] = [
-        {
-            id: 'cpu',
-            color: '#8DC6BC',
-            data: load.map((yValue, index: number) => ({
-                x: index + 1,
-                y: yValue,
-            })),
-        },
-    ]
-    const currentLoad =
-        chartData[0].data.length > 0 ? chartData[0].data[chartData[0].data.length - 1].y : 0
+  const chartData: ChartData[] = [
+    {
+      id: 'cpu',
+      color: '#8DC6BC',
+      data: load.map((yValue, index: number) => ({
+        x: index + 1,
+        y: yValue,
+      })),
+    },
+  ]
+  const currentLoad =
+    chartData[0].data.length > 0 ? chartData[0].data[chartData[0].data.length - 1].y : 0
 
-    const message = currentLoad < 80 ? 'Good' : 'Poor'
+  const message = currentLoad < 80 ? 'Good' : 'Poor'
 
-    return (
-        <Shadow
-            variant="$2"
-            style={{
-                width: '284px',
-                height: '136px',
-                borderRadius: '16px',
-                border: message === 'Poor' ? '1px solid  #D92344' : 'none',
-                backgroundColor: message === 'Poor' ? '#fefafa' : '#fff',
-            }}
+  return (
+    <Shadow
+      variant="$2"
+      style={{
+        width: '284px',
+        height: '136px',
+        borderRadius: '16px',
+        border: message === 'Poor' ? '1px solid  #D92344' : 'none',
+        backgroundColor: message === 'Poor' ? '#fefafa' : '#fff',
+      }}
+    >
+      <YStack>
+        <XStack
+          justifyContent="space-between"
+          style={{
+            padding: '8px 16px',
+            position: 'relative',
+          }}
         >
-            <YStack>
-                <XStack
-                    justifyContent="space-between"
-                    style={{
-                        padding: '8px 16px',
-                        position: 'relative',
-                    }}
-                >
-                    <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
-                        <StandartLineChart data={chartData} />
-                    </div>
-                    <YStack space={'$3'}>
-                        <Paragraph color={'#09101C'} size={'$6'} fontWeight={'600'}>
-                            CPU
-                        </Paragraph>
-                        <Paragraph color={'#09101C'} size={'$8'} fontWeight={'700'}>
-                            {currentLoad} GB
-                        </Paragraph>
-                    </YStack>
-                </XStack>
-                <Separator borderColor={'#e3e3e3'} />
-                <XStack space={'$4'} style={{ padding: '10px 16px 10px 16px' }}>
-                    <IconText
-                        icon={message === 'Good' ? <Icon src='icons/active.svg' width={16} /> : <IncorrectIcon size={16} />}
-                        weight={'semibold'}
-                    >
-                        {message}
-                    </IconText>
-                    {message === 'Poor' && (
-                        <Text size={13} color="#E95460">
-                            {((currentLoad / 80) * 100).toFixed(0)}% Utilization
-                        </Text>
-                    )}
-                </XStack>
-            </YStack>
-        </Shadow >
-    )
+          <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}>
+            <StandartLineChart data={chartData} />
+          </div>
+          <YStack space={'$3'}>
+            <Paragraph color={'#09101C'} size={'$6'} fontWeight={'600'}>
+              CPU
+            </Paragraph>
+            <Paragraph color={'#09101C'} size={'$8'} fontWeight={'700'}>
+              {currentLoad} GB
+            </Paragraph>
+          </YStack>
+        </XStack>
+        <Separator borderColor={'#e3e3e3'} />
+        <XStack space={'$4'} style={{ padding: '10px 16px 10px 16px' }}>
+          <IconText
+            icon={
+              message === 'Good' ? (
+                <Icon src="icons/active.svg" width={16} />
+              ) : (
+                <IncorrectIcon size={16} />
+              )
+            }
+            weight={'semibold'}
+          >
+            {message}
+          </IconText>
+          {message === 'Poor' && (
+            <Text size={13} color="#E95460">
+              {((currentLoad / 80) * 100).toFixed(0)}% Utilization
+            </Text>
+          )}
+        </XStack>
+      </YStack>
+    </Shadow>
+  )
 }
 
 export default CPUCard
--- src/pages/Dashboard/UptimeChart/UptimeChart.tsx
+++ src/pages/Dashboard/UptimeChart/UptimeChart.tsx
@@ -1,31 +1,28 @@
-
-import { BarChart, Bar,  ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, ResponsiveContainer } from 'recharts'
 // @NOTE: XAxis for months or days
 // @NOTE: XAxis, YAxis, CartesianGrid, Tooltip, Legend, = You can use in wider chart
 
-interface DataItem {
-    name?: string;
-    pa: number;
-    pv: number;
+type DataItem = {
+  name?: string
+  pa: number
+  pv: number
 }
 
 type UptimeChartProps = {
-    data: DataItem[];
+  data: DataItem[]
 }
 
 const UptimeChart = ({ data }: UptimeChartProps) => {
-    return (
-        <ResponsiveContainer  >
-            <BarChart
-                data={data}
-                style={{ backgroundColor: '#F0F2F5' }}
-            >
-                {/* <XAxis dataKey="name" fontSize={'10px'} width={50} /> */}
-                <Bar dataKey="pv" barSize={2} fill="#E95460" />
-                {/* <XAxis dataKey="name" fontSize={'10px'} width={50} /> */}
-                <Bar dataKey="pa" barSize={2} fill="#E95460" />
-            </BarChart>
-        </ResponsiveContainer>
-    );
+  return (
+    <ResponsiveContainer>
+      <BarChart data={data} style={{ backgroundColor: '#F0F2F5' }}>
+        {/* <XAxis dataKey="name" fontSize={'10px'} width={50} /> */}
+        <Bar dataKey="pv" barSize={2} fill="#E95460" />
+        {/* <XAxis dataKey="name" fontSize={'10px'} width={50} /> */}
+        <Bar dataKey="pa" barSize={2} fill="#E95460" />
+      </BarChart>
+    </ResponsiveContainer>
+  )
 }
-export default UptimeChart;
+
+export default UptimeChart
