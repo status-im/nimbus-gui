04e1ad313a4e23092eedfa433c621c1e3cc3e7c4
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-18 18:54:15 +0300

    fix: add valid icon for title logo

--- src/pages/Dashboard/TitleLogo.tsx
+++ src/pages/Dashboard/TitleLogo.tsx
@@ -1,10 +1,16 @@
-import { Text } from '@status-im/components'
+import { Avatar, Text } from '@status-im/components'
 import { XStack, YStack } from 'tamagui'
 
 const TitleLogo = () => {
   return (
     <XStack>
-      {/* <Avatar type="icon" size={80} icon={<img src={'/icons/dashboard-logo.png'} />} /> */}
+      <Avatar
+        type="icon"
+        size={32}
+        icon={
+          <img src={'/icons/dashboard-logo.png'} alt="dashboard-logo" style={{ width: '100%' }} />
+        }
+      />
       <YStack>
         <Text size={27} color="#09101C" weight={'semibold'}>
           Nimbus
