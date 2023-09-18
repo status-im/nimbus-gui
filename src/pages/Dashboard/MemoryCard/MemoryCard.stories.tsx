396dd909bd7ded4d1321ac41658f56b2520afe0d
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-18 19:38:31 +0300

    Add  badStats to memoryCard

--- src/pages/Dashboard/MemoryCard/MemoryCard.stories.tsx
+++ src/pages/Dashboard/MemoryCard/MemoryCard.stories.tsx
@@ -17,3 +17,6 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
     args: { currentMemory: [12, 24, 12, 4, 30, 50], maxMemory: 100 },
 }
+export const BadStats: Story = {
+    args: { maxMemory: 60, currentMemory: [2, 5, 7, 32, 23, 12, 89] }
+}
