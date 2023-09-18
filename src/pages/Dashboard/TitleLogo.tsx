085dc7027b0c7e502276f0e2eeef2fde31bbef13
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-18 19:14:03 +0300

    feat: style title logo component

--- src/pages/Dashboard/TitleLogo.tsx
+++ src/pages/Dashboard/TitleLogo.tsx
@@ -1,16 +1,18 @@
 import { Avatar, Text } from '@status-im/components'
-import { XStack, YStack } from 'tamagui'
+import { Stack, XStack, YStack } from 'tamagui'
 
 const TitleLogo = () => {
   return (
-    <XStack>
-      <Avatar
-        type="icon"
-        size={32}
-        icon={
-          <img src={'/icons/dashboard-logo.png'} alt="dashboard-logo" style={{ width: '100%' }} />
-        }
-      />
+    <XStack space={'$2'}>
+      <Stack style={{ marginTop: '3px' }}>
+        <Avatar
+          type="icon"
+          size={32}
+          icon={
+            <img src={'/icons/dashboard-logo.png'} alt="dashboard-logo" style={{ width: '100%' }} />
+          }
+        />
+      </Stack>
       <YStack>
         <Text size={27} color="#09101C" weight={'semibold'}>
           Nimbus
