24e72e020dbaed7586a2f01e3a8dd0d345d99bd8
Ivana Andersson <ivana.p.kostova@gmail.com>
2023-09-18 10:12:44 +0300

    feat: add Left sidebar component

---
+++ src/pages/Dashboard/LeftSidebar.tsx
@@ -0,0 +1,29 @@
+import { IconButton } from '@status-im/components'
+import { NodeIcon } from '@status-im/icons'
+import { XStack, YStack } from 'tamagui'
+
+const LeftSidebar = () => {
+  return (
+    <YStack
+      space={'$4'}
+      minHeight={'100vh'}
+      background={'#FFF'}
+      style={{
+        padding: '8px',
+        borderTopLeftRadius: '16px',
+        borderBottomLeftRadius: '16px',
+        border: '1px solid #F0F2F5',
+      }}
+    >
+      <IconButton icon={<NodeIcon size={20} />} variant="ghost" />
+      <IconButton icon={<NodeIcon size={20} />} variant="ghost" disabled />
+      <IconButton icon={<NodeIcon size={20} />} variant="ghost" selected />
+      <IconButton icon={<NodeIcon size={20} />} variant="ghost" />
+      <IconButton icon={<NodeIcon size={20} />} variant="ghost" />
+      <IconButton icon={<NodeIcon size={20} />} variant="ghost" />
+      <IconButton icon={<NodeIcon size={20} />} variant="ghost" />
+    </YStack>
+  )
+}
+
+export default LeftSidebar
