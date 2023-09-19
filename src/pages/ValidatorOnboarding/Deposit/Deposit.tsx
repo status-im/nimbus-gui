acc92df9eff241e740b13fb6cd5daadbfa1aec32
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-19 17:29:23 +0300

    feat: add styled YStack

--- src/pages/ValidatorOnboarding/Deposit/Deposit.tsx
+++ src/pages/ValidatorOnboarding/Deposit/Deposit.tsx
@@ -1,7 +1,12 @@
-import { Stack } from 'tamagui'
+import { YStack } from 'tamagui'
 
 const Deposit = () => {
-  return <Stack></Stack>
+  return (
+    <YStack
+      space={'$2'}
+      style={{ width: '100%', padding: '16px 32px', alignItems: 'start' }}
+    ></YStack>
+  )
 }
 
 export default Deposit
