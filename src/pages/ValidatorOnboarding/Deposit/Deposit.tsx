7ae10ab0113c20e1bfdc2e08f2d27d274ea5f54e
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-19 21:52:43 +0300

    feat: add eth and usd parts of deposit

--- src/pages/ValidatorOnboarding/Deposit/Deposit.tsx
+++ src/pages/ValidatorOnboarding/Deposit/Deposit.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Button, DividerLine, InformationBox, Input, Text } from '@status-im/components'
-import { PlaceholderIcon, AddIcon } from '@status-im/icons'
+import { PlaceholderIcon, AddIcon, ChevronDownIcon } from '@status-im/icons'
 import { Stack, XStack, YStack } from 'tamagui'
 import { useState } from 'react'
 import ValidatorRequest from './ValidatorRequest'
@@ -27,19 +27,39 @@ const Deposit = () => {
       <Text size={19} weight={'semibold'}>
         Deposit Funds
       </Text>
-      <Stack space={'$2'}>
-        <Text size={15} weight="regular" color={'#647084'}>
-          Connect you Wallet to stake required ETH for new validators
-        </Text>
-        <Input
-          icon={<AddIcon size={16} style={{ cursor: 'pointer' }} onClick={addValidatorHandler} />}
-          style={{ fontWeight: 'bold' }}
-          value={validatorCount.toString()}
-          onChange={changeValidatorCountHandler}
-        />
-      </Stack>
+      <XStack justifyContent={'space-between'} width={'80%'}>
+        <Stack space={'$2'}>
+          <Text size={15} weight="regular" color={'#647084'}>
+            Connect you Wallet to stake required ETH for new validators
+          </Text>
+          <Input
+            icon={<AddIcon size={16} style={{ cursor: 'pointer' }} onClick={addValidatorHandler} />}
+            style={{ fontWeight: 'bold' }}
+            value={validatorCount.toString()}
+            onChange={changeValidatorCountHandler}
+          />
+        </Stack>
+        <YStack space={'$2'}>
+          <Text size={15} weight={'semibold'} color="#09101C">
+            ETH
+          </Text>
+          <Text size={27} weight={'semibold'} color="#09101C">
+            64
+          </Text>
+        </YStack>
+        <YStack space={'$2'}>
+          <XStack style={{ justifyContent: 'space-between' }}>
+            <Text size={15} weight={'semibold'} color="#09101C">
+              USD
+            </Text>
+            <ChevronDownIcon size={16} color={'#919191'} />
+          </XStack>
+          <Text size={27} weight={'semibold'} color="#09101C">
+            $4,273 USD
+          </Text>
+        </YStack>
+      </XStack>
       <DividerLine />
-
       <ValidatorRequest />
       <ValidatorRequest />
       {isInfoBoxVisible && (
