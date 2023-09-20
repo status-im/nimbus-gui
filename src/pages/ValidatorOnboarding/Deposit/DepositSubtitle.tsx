fcf2020037766298f8975f407b0d01b46a17f3b8
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-20 22:43:04 +0300

    feat: create subtitle component

---
+++ src/pages/ValidatorOnboarding/Deposit/DepositSubtitle.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@status-im/components'
+
+const DepositSubtitle = () => {
+  return (
+    <Text size={15} weight="regular" color={'#647084'}>
+      Connect you Wallet to stake required ETH for new validators
+    </Text>
+  )
+}
+
+export default DepositSubtitle
