71dcb2aa7f89f6a624cce898af35b619629c06c8
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-20 21:13:28 +0300

    fix: remove not used components from connect wallet

--- src/pages/ValidatorOnboarding/Deposit/ConnectWallet.tsx
+++ src/pages/ValidatorOnboarding/Deposit/ConnectWallet.tsx
@@ -1,20 +1,18 @@
-import { Avatar, Button, Text } from '@status-im/components'
-import { XStack, YStack } from 'tamagui'
+import { Avatar, Button } from '@status-im/components'
+import { XStack } from 'tamagui'
 
 const ConnectWallet = () => {
   const onConnectWalletClick = () => {}
 
   return (
-    <YStack space={'$3'}>
-      <XStack space={'$2'} alignItems={'center'}>
-        <Avatar
-          type="icon"
-          size={32}
-          icon={<img src={'/icons/eth-logo.svg'} alt="eth-logo" style={{ width: '100%' }} />}
-        />
-        <Button onPress={onConnectWalletClick}>Connect Wallet</Button>
-      </XStack>
-    </YStack>
+    <XStack space={'$2'} alignItems={'center'}>
+      <Avatar
+        type="icon"
+        size={32}
+        icon={<img src={'/icons/eth-logo.svg'} alt="eth-logo" style={{ width: '100%' }} />}
+      />
+      <Button onPress={onConnectWalletClick}>Connect Wallet</Button>
+    </XStack>
   )
 }
 
