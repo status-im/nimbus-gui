0236408a258c5b642f3a455801ddd21b62599ea5
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-21 10:32:40 +0300

    Create SyncStatusCards.stories.tsx

---
+++ src/pages/Dashboard/SyncStatusCards/SyncStatusCards.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import SyncStatusCards from './SyncStatusCards'
+
+const meta = {
+    title: 'Dashboard/SyncStatusCards',
+    component: SyncStatusCards,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof SyncStatusCards>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {},
+}
