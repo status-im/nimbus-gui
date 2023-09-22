c8397227826f27378252b90503f7435c83864321
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-22 17:15:43 +0300

    fix: add args to val request story

--- src/pages/ValidatorOnboarding/Deposit/ValidatorRequest.stories.ts
+++ src/pages/ValidatorOnboarding/Deposit/ValidatorRequest.stories.ts
@@ -15,5 +15,10 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    validator: {
+      name: '1',
+      address: 'zQ3asdf9d4Gs0',
+    },
+  },
 }
