b4f57332fb7a00e2045d90cf166b874a3fde454c
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-22 15:34:13 +0300

    feat: add parts of validator request

--- src/pages/ValidatorOnboarding/Deposit/ValidatorRequest.tsx
+++ src/pages/ValidatorOnboarding/Deposit/ValidatorRequest.tsx
@@ -1,12 +1,19 @@
-import { DividerLine } from '@status-im/components'
-import { XStack } from 'tamagui'
+import { DividerLine, Text } from '@status-im/components'
+import { XStack, YStack } from 'tamagui'
 
 const ValidatorRequest = () => {
   return (
-    <>
-      <XStack space={'$2'}></XStack>
+    <YStack space={'$3'} style={{ width: '100%' }}>
+      <XStack style={{ justifyContent: 'space-between' }}>
+        <Text size={13} color="#647084" weight={'semibold'}>
+          Keys Generated
+        </Text>
+        <Text size={13} color="#2F80ED" weight={'semibold'}>
+          Requires Deposit
+        </Text>
+      </XStack>
       <DividerLine />
-    </>
+    </YStack>
   )
 }
 
