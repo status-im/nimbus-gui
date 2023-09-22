f6de3a708c677022f8deea694fc10e1cd599106c
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-22 17:35:16 +0300

    Create ClientAddressRow.stories.tsx

---
+++ src/pages/ConnectExistingInstance/ClientAddressRow.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ClientAddressRow from './ClientAddressRow'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+const meta = {
+    title: 'Connect-Device/ClientAddressRow',
+    component: ClientAddressRow,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    decorators: [withRouter],
+} satisfies Meta<typeof ClientAddressRow>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Page: Story = {
+    args: {},
+}
