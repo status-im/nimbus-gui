eff6fc4a75cb3e297fba06d3f6139b013c3b7a2d
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-22 09:21:51 +0300

    feat: add redux and wallet connected state

---
+++ src/redux/ValidatorOnboarding/Deposit/slice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+type DepositState = {
+  isWalletConnected: boolean
+}
+
+const initialState: DepositState = {
+  isWalletConnected: false,
+}
+
+const depositSlice = createSlice({
+  name: 'deposit',
+  initialState,
+  reducers: {
+    setIsWalletConnected: (state, action) => {
+      state.isWalletConnected = action.payload
+    },
+  },
+})
+
+export const { setIsWalletConnected } = depositSlice.actions
+
+export default depositSlice.reducer
