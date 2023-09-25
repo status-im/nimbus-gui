91b91ddb88706986cf3ae5064f3521358da0e3a7
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-25 22:20:55 +0300

    Style  validator Address Switch and Switch thumb

--- src/pages/ConnectExistingInstance/ClientAddressRow.tsx
+++ src/pages/ConnectExistingInstance/ClientAddressRow.tsx
@@ -16,9 +16,8 @@ const ClientAddressRow = () => {
                         <Text size={13} color={'#647084'} weight={'semibold'}> Protocol </Text>
                         <Text size={11} color={'#647084'} weight={'regular'}> (HTTP/HTTPS)</Text>
                     </YStack>
-
-                    <Switch size="$2" style={isBeaconSwitchOn ? { backgroundColor: '#2A4AF5' } : { backgroundColor: 'grey' }} checked={isBeaconSwitchOn} onCheckedChange={() => setIsBeaconSwitchOn(prev => !prev)}>
-                        <Switch.Thumb style={{ backgroundColor: '#fff', border: '1px solid #2A4AF5' }} />
+                    <Switch size="$1" style={isBeaconSwitchOn ? { backgroundColor: '#2A4AF5' } : { backgroundColor: 'grey' }} checked={isBeaconSwitchOn} onCheckedChange={() => setIsBeaconSwitchOn(prev => !prev)}>
+                        <Switch.Thumb style={{ right: 7, bottom: 3, backgroundColor: '#fff', height: '16px', width: '16px' }} />
                     </Switch>
                 </YStack>
                 <YStack space={'$2'}>
