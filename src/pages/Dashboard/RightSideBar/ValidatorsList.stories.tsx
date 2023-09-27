a9bb6fcaf3280b310d8a4346baab928f1e2f0440
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-27 11:43:22 +0300

    Create ValidatorsList.stories.tsx

---
+++ src/pages/Dashboard/RightSideBar/ValidatorsList.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ValidatorsList from './ValidatorsList'
+
+const meta = {
+    title: 'Dashboard/ValidatorsList',
+    component: ValidatorsList,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof ValidatorsList>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+    args: {},
+}
