2c3e4faa3e04742287e641544ada1538a4646229
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-09-27 16:50:59 +0300

    feat: reuse validator menu with price for client setup

--- src/pages/ValidatorOnboarding/ClientSetup/SetupRow.tsx
+++ src/pages/ValidatorOnboarding/ClientSetup/SetupRow.tsx
@@ -1,7 +1,8 @@
-import { Stack, XStack, YStack } from 'tamagui'
-import { Input as StatusInput, Text } from '@status-im/components'
-import { AddIcon, ChevronDownIcon } from '@status-im/icons'
+import { YStack } from 'tamagui'
+import { Text } from '@status-im/components'
 import { useState } from 'react'
+
+import ValidatorsMenuWithPrice from '../../../components/General/ValidatorsMenuWithPrice'
 
 type SetupRowProps = {
   title: string
@@ -9,10 +10,6 @@ type SetupRowProps = {
 
 const SetupRow = ({ title }: SetupRowProps) => {
   const [validatorCount, setValidatorCount] = useState(0)
-
-  const addValidatorHandler = () => {
-    setValidatorCount((state: number) => state + 1)
-  }
 
   const changeValidatorCountHandler = (value: string) => {
     const numberValue = Number(value)
@@ -26,38 +23,11 @@ const SetupRow = ({ title }: SetupRowProps) => {
       <Text size={19} weight={'semibold'}>
         {title}
       </Text>
-      <XStack justifyContent={'space-between'} width={'80%'}>
-        <Stack space={'$2'}>
-          <Text size={15} weight="regular" color={'#647084'}>
-            How many Validators would you like to run?
-          </Text>
-          <StatusInput
-            icon={<AddIcon size={16} style={{ cursor: 'pointer' }} onClick={addValidatorHandler} />}
-            value={validatorCount.toString()}
-            onChangeText={changeValidatorCountHandler}
-            style={{ fontWeight: 'bold' }}
-          />
-        </Stack>
-        <YStack space={'$2'}>
-          <Text size={15} weight={'semibold'} color="#09101C">
-            ETH
-          </Text>
-          <Text size={27} weight={'semibold'} color="#09101C">
-            64
-          </Text>
-        </YStack>
-        <YStack space={'$2'}>
-          <XStack style={{ justifyContent: 'space-between' }}>
-            <Text size={15} weight={'semibold'} color="#09101C">
-              USD
-            </Text>
-            <ChevronDownIcon size={16} color={'#919191'} />
-          </XStack>
-          <Text size={27} weight={'semibold'} color="#09101C">
-            $4,273 USD
-          </Text>
-        </YStack>
-      </XStack>
+      <ValidatorsMenuWithPrice
+        validatorCount={validatorCount}
+        changeValidatorCountHandler={changeValidatorCountHandler}
+        label="How many Validators would you like to run?"
+      />
     </YStack>
   )
 }
