b7643197fd219a59076abdeb6e9c542d18f5a8cb
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-09-28 22:53:56 +0300

    add a light stroke to the normal Device memorycard

--- src/components/Charts/DeviceMemoryHealth.tsx
+++ src/components/Charts/DeviceMemoryHealth.tsx
@@ -51,7 +51,7 @@ const DeviceMemoryHealth = ({ currentMemory, maxMemory }: DeviceMemoryHealthProp
         width: '50%',
         minHeight: '135px',
         borderRadius: '16px',
-        border: message === 'Poor' ? '1px solid  #D92344' : 'none',
+        border: message === 'Poor' ? '1px solid #D92344' : '1px solid #E0E0E0',  
         backgroundColor: isHovered ? '#f8f6ff' : (message === 'Poor' ? '#fefafa' : '#fff'),
       }}
       onMouseEnter={() => setIsHovered(true)}
