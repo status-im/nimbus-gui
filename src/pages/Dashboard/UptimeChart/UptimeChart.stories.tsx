f66a848b69a166a357c49169586a0130111ea740
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-10-06 11:08:24 +0300

    fix story

--- src/pages/Dashboard/UptimeChart/UptimeChart.stories.tsx
+++ src/pages/Dashboard/UptimeChart/UptimeChart.stories.tsx
@@ -10,7 +10,7 @@ const meta = {
   },
   decorators: [
     Story => (
-      <div style={{ height: '20vh', width: '50vh' }}>
+      <div style={{ height: '20vh', width: '90vh' }}>
         <Story />
       </div>
     ),
@@ -21,104 +21,9 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const WithoutLabels: Story = {
-  args: {
-    data: [
-      {
-        pv: 1,
-        pa: 1,
-      },
-      {
-        pv: 0,
-        pa: 0,
-      },
-      {
-        pv: 0,
-        pa: 0,
-      },
-      {
-        pv: 0,
-        pa: 1,
-      },
-      {
-        pv: 0,
-        pa: 0,
-      },
-      {
-        pv: 1,
-        pa: 0,
-      },
-      {
-        pv: 0,
-        pa: 1,
-      },
-    ],
-  },
-}
-
 export const WithMonths: Story = {
   args: {
-    data: [
-      {
-        name: 'Jan',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Feb',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Mar',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Apr',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'May',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Aug',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Jun',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Jul',
-        pv: 1,
-        pa: 0,
-      },
-      {
-        name: 'Sep',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Oct',
-        pv: 0,
-        pa: 0,
-      },
-      {
-        name: 'Nov',
-        pv: 1,
-        pa: 1,
-      },
-      {
-        name: 'Dec',
-        pv: 1,
-        pa: 1,
-      },
-    ],
+
+    monthlyActivity:[3, 2, 5, 4, 6, 7, 8, 0, 10, 0, 0, 3], startMonth: 0, endMonth: 12, withLabels: true
   },
 }
