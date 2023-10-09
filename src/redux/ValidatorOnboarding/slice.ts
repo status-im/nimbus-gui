6567d76b8f569863808503ebf36422b0093dc810
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-09 16:35:36 +0300

    feat: create slice for validator on boarding

---
+++ src/redux/ValidatorOnboarding/slice.ts
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+type ValidatorOnboardingState = {}
+
+const initialState: ValidatorOnboardingState = {}
+
+const validatorOnboardingSlice = createSlice({
+  name: 'validatorOnboarding',
+  initialState,
+  reducers: {},
+})
+
+export const {} = validatorOnboardingSlice.actions
+
+export default validatorOnboardingSlice.reducer
