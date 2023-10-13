141a3a96f9fdcab44546c3a1ab7e2c649790a0af
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-10-13 12:53:40 +0300

    clear width from dashboard components

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -17,52 +17,52 @@ import MemoryCard from './MemoryCard/MemoryCard'
 
 const Dashboard = () => {
   return (
-      <XStack justifyContent={'space-between'}  >
-        <LeftSidebar />
+    <XStack justifyContent={'space-between'}  >
+      <LeftSidebar />
 
-        <YStack
-          space={'$4'}
-          alignItems="start"
-          px="24px"
-          style={{ flexGrow: '1', marginTop: '16px', overflowY: 'scroll' }}
-        >
-          <TitleLogo />
-          <XStack space={'$4'} justifyContent={'space-between'} width={'100%'}>
-            <XStack space={'$4'} width={'40%'}>
-              <SyncStatusCard />
-              <AddCardsContainer />
+      <YStack
+        space={'$4'}
+        alignItems="start"
+        px="24px"
+        style={{ flexGrow: '1', marginTop: '16px' }}
+      >
+        <TitleLogo />
+        <XStack space={'$4'} justifyContent={'space-between'} >
+          <XStack space={'$4'}  >
+            <SyncStatusCard />
+            <AddCardsContainer />
+          </XStack>
+          <BalanceChartCard />
+        </XStack>
+        <BasicInfoCards />
+
+        <XStack space="$3"  >
+          <YStack space={'$4'}  >
+            <XStack justifyContent="space-between">
+              <ConsensusUptimeCard />
+              <ExecutionUptime />
             </XStack>
-            <BalanceChartCard />
-          </XStack>
-          <BasicInfoCards />
+            <DeviceUptime />
+          </YStack>
+          <YStack space={'$4'}  >
+            <XStack space="$4"  >
+              <StorageCard maxStorage={100} storage={82} />
+              <CPUCard load={[12, 31, 3, 2, 24, 98]} />
+            </XStack>
+            <XStack space="$4"  >
+              <MemoryCard currentMemory={[21, 33, 3, 42, 35]} maxMemory={50} />
+              <NetworkCard
+                downloadRate={[12, 31, 22, 12, 23, 23, 90]}
+                uploadRate={[31, 22, 32, 132, 32, 45, 65]}
+              />
+            </XStack>
+          </YStack>
+        </XStack>
+      </YStack>
 
-          <XStack space="$3" width={'100%'}>
-            <YStack space={'$4'} width={'50%'}>
-              <XStack justifyContent="space-between">
-                <ConsensusUptimeCard />
-                <ExecutionUptime />
-              </XStack>
-              <DeviceUptime />
-            </YStack>
-            <YStack space={'$4'} width={'50%'}>
-              <XStack space="$4" width={'100%'}>
-                <StorageCard maxStorage={100} storage={82} />
-                <CPUCard load={[12, 31, 3, 2, 24, 98]} />
-              </XStack>
-              <XStack space="$4" width={'100%'}>
-                <MemoryCard currentMemory={[21, 33, 3, 42, 35]} maxMemory={50} />
-                <NetworkCard
-                  downloadRate={[12, 31, 22, 12, 23, 23, 90]}
-                  uploadRate={[31, 22, 32, 132, 32, 45, 65]}
-                />
-              </XStack>
-            </YStack>
-          </XStack>
-        </YStack>
+      <RightSidebar />
+    </XStack>
 
-        <RightSidebar />
-      </XStack>
-    
   )
 }
 
