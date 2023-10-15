a59b34e019a28922379159857f3b6554a1b547bb
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-15 22:40:48 +0300

    fix: add valid info badge icon to key gen sync card

--- src/pages/ValidatorOnboarding/KeyGeneration/KeyGenerationHeader/KeyGenerationSyncCard.tsx
+++ src/pages/ValidatorOnboarding/KeyGeneration/KeyGenerationHeader/KeyGenerationSyncCard.tsx
@@ -1,5 +1,5 @@
 import { Stack, XStack, YStack } from 'tamagui'
-import { ClearIcon } from '@status-im/icons'
+import { InfoBadgeIcon } from '@status-im/icons'
 import { Text } from '@status-im/components'
 
 import StandardGauge from '../../../../components/Charts/StandardGauge'
@@ -48,7 +48,7 @@ const KeyGenerationSyncCard = ({ synced, total, title, color }: KeyGenerationSyn
             {formatNumbersWithComa(synced)} / {formatNumbersWithComa(total)}
           </Text>
         </YStack>
-        <ClearIcon size={20} color="#A1ABBD" style={{ cursor: 'pointer' }} />
+        <InfoBadgeIcon size={20} color="#A1ABBD" style={{ cursor: 'pointer' }} />
       </XStack>
     </BorderBox>
   )
