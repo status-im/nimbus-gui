1041b08454b554f62620f8c15aa54be74f11b0f4
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-17 19:20:08 +0300

    feat: add folder for left sidebar and components

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,8 @@
 import { XStack } from 'tamagui'
 
-import LeftSidebar from './LeftSidebar'
-import RightSidebar from './RightSideBar/RightSidebar'
+import LeftSidebar from './LeftSidebar/LeftSidebar'
 import DashboardContent from './DashboardContent'
+import RightSidebar from './RightSideBar/RightSidebar'
 
 const Dashboard = () => (
   <XStack style={{ height: '100vh' }}>
