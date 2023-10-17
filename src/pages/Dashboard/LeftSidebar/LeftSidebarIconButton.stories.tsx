a0ffd1df3fb6f90d25e4a22bb3322743d00f5215
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-17 19:49:06 +0300

    feat: add all examples for left sidebar icon story

--- src/pages/Dashboard/LeftSidebar/LeftSidebarIconButton.stories.tsx
+++ src/pages/Dashboard/LeftSidebar/LeftSidebarIconButton.stories.tsx
@@ -1,5 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { DashboardIcon } from '@status-im/icons'
+import {
+  DashboardIcon,
+  SpeedIcon,
+  ChartIcon,
+  HeartIcon,
+  CodeBlockIcon,
+  CommunitiesIcon,
+  ActivityCenterIcon,
+  SettingsIcon,
+} from '@status-im/icons'
 
 import LeftSidebarIconButton from './LeftSidebarIconButton'
 
@@ -18,10 +27,142 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     iconEl: <DashboardIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: false,
+    id: 'dashboard',
+  },
+}
+
+export const Outlined: Story = {
+  args: {
+    iconEl: <DashboardIcon size={20} />,
+    variant: 'outline',
+    isDotOn: false,
+    selected: false,
+    disabled: false,
+    id: 'dashboard',
+  },
+}
+
+export const WithDot: Story = {
+  args: {
+    iconEl: <DashboardIcon size={20} />,
     variant: 'outline',
     isDotOn: true,
+    selected: false,
+    disabled: false,
+    id: 'dashboard',
+  },
+}
+
+export const Selected: Story = {
+  args: {
+    iconEl: <DashboardIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
     selected: true,
     disabled: false,
     id: 'dashboard',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    iconEl: <DashboardIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: true,
+    id: 'dashboard',
+  },
+}
+
+export const Dashboard: Story = {
+  args: {
+    iconEl: <DashboardIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: true,
+    disabled: false,
+    id: 'dashboard',
+  },
+}
+
+export const Speed: Story = {
+  args: {
+    iconEl: <SpeedIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: false,
+    id: 'speed',
+  },
+}
+
+export const Chart: Story = {
+  args: {
+    iconEl: <ChartIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: true,
+    id: 'chart',
+  },
+}
+
+export const Heart: Story = {
+  args: {
+    iconEl: <HeartIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: false,
+    id: 'heart',
+  },
+}
+
+export const CodeBlock: Story = {
+  args: {
+    iconEl: <CodeBlockIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: false,
+    id: 'codeBlock',
+  },
+}
+
+export const Communities: Story = {
+  args: {
+    iconEl: <CommunitiesIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: false,
+    id: 'communities',
+  },
+}
+
+export const ActivityCenter: Story = {
+  args: {
+    iconEl: <ActivityCenterIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: true,
+    selected: false,
+    disabled: false,
+    id: 'activityCenter',
+  },
+}
+
+export const Settings: Story = {
+  args: {
+    iconEl: <SettingsIcon size={20} />,
+    variant: 'ghost',
+    isDotOn: false,
+    selected: false,
+    disabled: false,
+    id: 'settings',
   },
 }
