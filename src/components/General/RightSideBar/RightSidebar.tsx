5bb9ea72e0e8ab069ed065d7974e0b3595d9e6ab
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-18 07:56:01 +0300

    feat: add texts for diamond card

--- src/components/General/RightSideBar/RightSidebar.tsx
+++ src/components/General/RightSideBar/RightSidebar.tsx
@@ -32,23 +32,33 @@ const RightSidebar = () => {
           <Text size={13}>{getFormattedWalletAddress('0xb9dc35')}</Text>
         </YStack>
       </XStack>
-      <XStack alignItems="center" justifyContent="space-between">
+      <XStack space={'$2'} alignItems="center" justifyContent="space-between">
         <YStack
           backgroundColor={'#2A4AF5'}
           style={{
             padding: '8px 12px',
             borderRadius: '16px',
             flexGrow: '1',
-            marginRight: '8px',
-            height: '100%',
+            height: 'max-fit',
           }}
         >
           <Text size={15} weight={'semibold'} color={'#FFF'}>
-            Diamond Hands
+            💎 Diamond Hands
           </Text>
           <Text size={27} weight={'semibold'} color={'#FFF'}>
             $0.00
           </Text>
+          <XStack space={'$1.5'} alignItems={'center'}>
+            <Text size={13} color={'#bac7fb'}>
+              0%
+            </Text>
+            <Text size={13} color={'#bac7fb'}>
+              .
+            </Text>
+            <Text size={13} color={'#bac7fb'}>
+              $0
+            </Text>
+          </XStack>
         </YStack>
         <AddCard padding={'0 2vw'} />
       </XStack>
