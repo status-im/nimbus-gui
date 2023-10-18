b8e43a986b8c5b8145de0596565af16f7eccc966
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-18 09:39:38 +0300

    feat: add state and effect for alerts

--- src/components/General/RightSideBar/AlertsList.tsx
+++ src/components/General/RightSideBar/AlertsList.tsx
@@ -1,8 +1,33 @@
 import { InformationBox, Text } from '@status-im/components'
 import { XStack, YStack } from 'tamagui'
 import { CloseCircleIcon, ChevronRightIcon } from '@status-im/icons'
+import { useEffect, useState } from 'react'
+
+type Alert = {
+  message: string
+  variant: 'default' | 'information' | 'error'
+}
 
 const AlertsList = () => {
+  const [_, setAlerts] = useState([] as Alert[])
+
+  useEffect(() => {
+    setAlerts([
+      {
+        message: 'Network: Participation rate below 66%',
+        variant: 'default',
+      },
+      {
+        message: 'Node: 32 Peers Connected',
+        variant: 'information',
+      },
+      {
+        message: 'Node / Validator: Process Down',
+        variant: 'error',
+      },
+    ])
+  }, [])
+
   return (
     <YStack space={'$2'}>
       <XStack justifyContent="space-between">
