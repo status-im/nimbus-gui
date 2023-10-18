2c87e15934fb63517afa0d8755f48c1d47868adc
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-18 16:11:26 +0300

    feat: add state and effect for logs

--- src/components/General/RightSideBar/LogsList.tsx
+++ src/components/General/RightSideBar/LogsList.tsx
@@ -1,8 +1,33 @@
 import { InformationBox, Text } from '@status-im/components'
 import { XStack, YStack } from 'tamagui'
 import { CloseCircleIcon, ChevronRightIcon } from '@status-im/icons'
+import { useEffect, useState } from 'react'
+
+type Log = {
+  message: string
+  variant: 'default' | 'information' | 'error'
+}
 
 const LogsList = () => {
+  const [logs, setLogs] = useState([] as Log[])
+
+  useEffect(() => {
+    setLogs([
+      {
+        message: 'Critical Logs: 0.01 / M',
+        variant: 'default',
+      },
+      {
+        message: 'Warning Logs: 0.01 / M',
+        variant: 'default',
+      },
+      {
+        message: 'Error Logs: 0.01 / M',
+        variant: 'default',
+      },
+    ])
+  }, [])
+
   return (
     <YStack space={'$2'}>
       <XStack justifyContent="space-between">
