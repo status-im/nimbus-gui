5c612d15e15408fea1d6195e16f961a2b19739e3
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-19 08:46:27 +0300

    feat: map tabs

--- src/components/General/RightSideBar/ValidatorsTabs/ValidatorsTabs.tsx
+++ src/components/General/RightSideBar/ValidatorsTabs/ValidatorsTabs.tsx
@@ -2,21 +2,32 @@ import { Tabs } from '@status-im/components'
 
 import ValidatorsList from './ValidatorsList'
 
+const VALIDATOR_TABS = [
+  {
+    label: 'Active',
+    value: 'active',
+  },
+  {
+    label: 'Pending',
+    value: 'pending',
+  },
+  {
+    label: 'Inactive',
+    value: 'inactive',
+  },
+]
+
 const ValidatorsTabs = () => {
   return (
     <Tabs defaultValue="active">
       <Tabs.List size={32}>
-        <Tabs.Trigger type="default" value="active">
-          Active
-        </Tabs.Trigger>
-        <Tabs.Trigger type="default" value="pending">
-          Pending
-        </Tabs.Trigger>
-        <Tabs.Trigger type="default" value="inactive">
-          Inactive
-        </Tabs.Trigger>
+        {VALIDATOR_TABS.map(tab => (
+          <Tabs.Trigger type="default" value={tab.value}>
+            {tab.label}
+          </Tabs.Trigger>
+        ))}
       </Tabs.List>
-      <Tabs.Content value="active">
+      <Tabs.Content value="active" style={{ marginTop: '8px' }}>
         <ValidatorsList />
       </Tabs.Content>
       <Tabs.Content value="pending">
