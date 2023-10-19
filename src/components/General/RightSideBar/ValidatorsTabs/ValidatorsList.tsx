6958b862bf704413553575e307580db556b86f45
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-19 09:24:07 +0300

    feat: create state for validators

--- src/components/General/RightSideBar/ValidatorsTabs/ValidatorsList.tsx
+++ src/components/General/RightSideBar/ValidatorsTabs/ValidatorsList.tsx
@@ -1,4 +1,4 @@
-import { Stack, YStack } from 'tamagui'
+import { YStack } from 'tamagui'
 import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
@@ -15,47 +15,46 @@ type Validator = {
 }
 
 const ValidatorsList = () => {
+  const [validators, setValidators] = useState([] as Validator[])
+  const [filteredValidators, setFilteredValidators] = useState([] as Validator[])
   const [searchValue, setSearchValue] = useState('')
   const dispatch = useDispatch()
-  const [filteredValidators, setFilteredValidators] = useState([] as Validator[])
-
-  const validators = [
-    {
-      name: 'Validator 1',
-      validatorAddress: 'zQ3asdf9d4Gs0',
-      selected: true,
-      isAvatarChipIncluded: true,
-      isVerified: true,
-    },
-    { name: 'Validator 2', validatorAddress: 'zQ3asdf9d4Gs0', isVerified: true },
-    { name: 'Validator 3', validatorAddress: 'zQ3asdf9d4Gs0', isAvatarChipIncluded: true },
-    { name: 'Validator 4', validatorAddress: 'zQ3asdf9d4Gs0' },
-    { name: 'Validator 5', validatorAddress: 'zQ3asdf9d4Gs0', isVerified: true },
-  ]
 
   useEffect(() => {
-    if (searchValue) {
-      const filtered = validators.filter(validator =>
-        validator.name.toLowerCase().includes(searchValue.toLowerCase()),
-      )
-      const filteredCount = filtered.length
-      dispatch(setCountOfValidators(filteredCount))
-      setFilteredValidators(filtered)
-    } else {
-      dispatch(setCountOfValidators(validators.length))
-      setFilteredValidators(validators)
-    }
-  }, [searchValue, validators])
+    setValidators([
+      {
+        name: '1',
+        validatorAddress: 'zQ3asdf9d4Gs0',
+        selected: true,
+        isAvatarChipIncluded: true,
+        isVerified: true,
+      },
+      { name: '2', validatorAddress: 'zQ3asdf9d4Gs0', isVerified: true },
+      { name: '3', validatorAddress: 'zQ3asdf9d4Gs0', isAvatarChipIncluded: true },
+      { name: '4', validatorAddress: 'zQ3asdf9d4Gs0' },
+      { name: '5', validatorAddress: 'zQ3asdf9d4Gs0', isVerified: true },
+    ])
+  }, [])
+
+  useEffect(() => {
+    const filtered = validators.filter(validator =>
+      validator.name.toLowerCase().includes(searchValue.toLowerCase()),
+    )
+    setFilteredValidators(filtered)
+
+    const filteredCount = filtered.length
+    dispatch(setCountOfValidators(filteredCount))
+  }, [searchValue])
 
   return (
-    <Stack>
+    <YStack>
       <InputSearch value={searchValue} setValue={setSearchValue} />
       <YStack space={'$1'} mt={'16px'}>
         {filteredValidators.map(validator => (
           <ValidatorListItem {...validator} />
         ))}
       </YStack>
-    </Stack>
+    </YStack>
   )
 }
 
