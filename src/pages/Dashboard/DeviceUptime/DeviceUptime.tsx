99d1728939d8d17956817643c65a55882296a003
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-10-20 14:43:38 +0300

    Update DeviceUptime.tsx

--- src/pages/Dashboard/DeviceUptime/DeviceUptime.tsx
+++ src/pages/Dashboard/DeviceUptime/DeviceUptime.tsx
@@ -28,7 +28,7 @@ const DeviceUptime = () => {
   }
 
   return (
-    <DashboardCardWrapper maxHeight="350px">
+    <DashboardCardWrapper maxHeight="100px"   >
       <YStack space={'$3'}>
         <XStack justifyContent={'space-between'}>
           <YStack>
