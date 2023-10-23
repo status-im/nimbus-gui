783db2806b3029b109a5d2efbdd5578f0134d7ca
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-23 20:40:25 +0300

    fix: mac constant and import order for stories

--- src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstalling/OsCard.stories.ts
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstalling/OsCard.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import OSCard from './OSCard'
+import { MAC } from '../../../../constants'
 
 const meta = {
   title: 'ValidatorOnboarding/OSCard',
--- src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstalling/SyntaxHighlighter.stories.tsx
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstalling/SyntaxHighlighter.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 import SyntaxHighlighter from './SyntaxHighlighter'
-import { withRouter } from 'storybook-addon-react-router-v6'
 
 const meta = {
   title: 'ValidatorOnboarding/SyntaxHighlighter',
