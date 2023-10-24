1704ca48844e692abf8be3872b7e6c81f5fd48eb
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-24 06:32:25 +0300

    feat: create constant for documenatation

--- src/constants.ts
+++ src/constants.ts
@@ -36,6 +36,35 @@ export const MAC = 'MacOS'
 export const WINDOWS = 'Windows'
 export const LINUX = 'Linux'
 
+type DocumentationItem =
+  | { type: 'code'; content: string }
+  | { type: 'link'; content: string }
+  | { type: 'text'; content: string }
+
+export const DOCUMENTATION: Record<string, Record<string, DocumentationItem[]>> = {
+  Nethermind: {
+    [MAC]: [{ type: 'text', content: 'Text for mac' }],
+    [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
+    [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+  },
+  Besu: {
+    [MAC]: [{ type: 'text', content: 'Text for mac' }],
+    [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
+    [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+  },
+  Geth: {
+    [MAC]: [{ type: 'text', content: 'Text for mac' }],
+    [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
+    [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+  },
+  Erigon: {
+    [MAC]: [{ type: 'text', content: 'Text for mac' }],
+    [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
+    [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+  },
+  Nimbus: {},
+}
+
 // for now, this will be constant values
 export const CURRENCIES = {
   USD: 1583.42,
