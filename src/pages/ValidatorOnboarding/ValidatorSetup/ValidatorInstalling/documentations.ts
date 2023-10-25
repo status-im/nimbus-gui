5a2d6af168e5a185a260665120193cf7092ca527
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-25 08:54:30 +0300

    fix: change documentation to new structure

--- src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstalling/documentations.ts
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ValidatorInstalling/documentations.ts
@@ -1,14 +1,9 @@
 import { LINUX, MAC, WINDOWS } from '../../../../constants'
 
-export type DocumentationItem =
-  | { type: 'code'; content: Array<string> }
-  | { type: 'link'; content: string; to: string }
-  | { type: 'text'; content: string }
-
 type ExecClientData = {
   icon: string
-  general: DocumentationItem[]
-  documentation: Record<string, DocumentationItem[]>
+  general: string
+  documentation: Record<string, string>
 }
 
 type Documentation = Record<string, ExecClientData>
@@ -16,26 +11,21 @@ type Documentation = Record<string, ExecClientData>
 export const DOCUMENTATIONS: Documentation = {
   Nethermind: {
     icon: '/icons/nethermind-circle.png',
-    general: [
-      {
-        type: 'text',
-        content:
-          'Ethereum’s long awaited shift from Proof of Work (POW) to Proof of Stake (POS) known as The Merge happened on September 15, 2022 and came with fundamental changes to the network. The most notable change is the addition of the Beacon chain (Consensus layer) which replaced Proof of Work mining. It is coordinating and pseudorandomly selecting block producers from the pool of stakers / validators in a way that makes it extremely difficult for validators to coordinate attacks on the network. The Merge changed how operators run nodes on the Ethereum blockchain. A node now needs two clients that work together as a pair. In addition to the Execution Layer client (e.g. Nethermind) you need a Consensus Layer client that connects to the Beacon chain and runs the POS algorithm. This guide will show you everything you need to know to operate an Ethereum node. It will show how to connect to the Goerli, Sepolia and Chiado test networks as well. An easy way to run both CL and EL clients is by using Sedge. Sedge is a setup tool for PoS network/chain validators and nodes. Currently, Sedge supports multiple Linux distributions and MacOS.',
-      },
-    ],
+    general:
+      'Ethereum’s long awaited shift from Proof of Work (POW) to Proof of Stake (POS) known as The Merge happened on September 15, 2022 and came with fundamental changes to the network. The most notable change is the addition of the Beacon chain (Consensus layer) which replaced Proof of Work mining. It is coordinating and pseudorandomly selecting block producers from the pool of stakers / validators in a way that makes it extremely difficult for validators to coordinate attacks on the network. The Merge changed how operators run nodes on the Ethereum blockchain. A node now needs two clients that work together as a pair. In addition to the Execution Layer client (e.g. Nethermind) you need a Consensus Layer client that connects to the Beacon chain and runs the POS algorithm. This guide will show you everything you need to know to operate an Ethereum node. It will show how to connect to the Goerli, Sepolia and Chiado test networks as well. An easy way to run both CL and EL clients is by using Sedge. Sedge is a setup tool for PoS network/chain validators and nodes. Currently, Sedge supports multiple Linux distributions and MacOS.',
     documentation: {
-      [MAC]: [{ type: 'text', content: 'Text for mac' }],
-      [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
-      [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+      [MAC]: 'Text for mac',
+      [WINDOWS]: 'Text for windows',
+      [LINUX]: 'Text for linux',
     },
   },
   Besu: {
     icon: '/icons/hyperledger-besu-circle.png',
-    general: [],
+    general: '',
     documentation: {
-      [MAC]: [{ type: 'text', content: 'Text for mac' }],
-      [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
-      [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+      [MAC]: 'Text for mac',
+      [WINDOWS]: 'Text for windows',
+      [LINUX]: 'Text for linux',
     },
   },
   Geth: {
@@ -112,26 +102,21 @@ export const DOCUMENTATIONS: Documentation = {
   },
   Erigon: {
     icon: '/icons/erigon-circle.png',
-    general: [
-      {
-        type: 'text',
-        content:
-          "In order to use Erigon, the software has to be installed first. There are several ways to install Erigon, depending on the operating system and the user's choice of installation method, e.g. using a package manager, container or building from source.",
-      },
-    ],
+    general:
+      "In order to use Erigon, the software has to be installed first. There are several ways to install Erigon, depending on the operating system and the user's choice of installation method, e.g. using a package manager, container or building from source.",
     documentation: {
-      [MAC]: [{ type: 'text', content: 'Text for mac' }],
-      [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
-      [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+      [MAC]: 'Text for mac',
+      [WINDOWS]: 'Text for windows',
+      [LINUX]: 'Text for linux',
     },
   },
   Nimbus: {
     icon: '/icons/NimbusDisabled.svg',
-    general: [],
+    general: '',
     documentation: {
-      [MAC]: [{ type: 'text', content: 'Text for mac' }],
-      [WINDOWS]: [{ type: 'text', content: 'Text for windows' }],
-      [LINUX]: [{ type: 'text', content: 'Text for linux' }],
+      [MAC]: 'Text for mac',
+      [WINDOWS]: 'Text for windows',
+      [LINUX]: 'Text for linux',
     },
   },
 }
