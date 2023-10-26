ca14a002f61d53b38168a2fb6d8d325ea0a19e1d
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-26 09:21:52 +0300

    fix: remove not used imports

--- src/components/General/Markdown/Markdown.tsx
+++ src/components/General/Markdown/Markdown.tsx
@@ -1,20 +1,13 @@
 import ReactMarkdown from 'react-markdown'
 
 import MarkdownLink from './MarkdownLink'
-import MarkdownText from './MarkdownText'
-import MarkdownCode from './MarkdownCode'
 
 type MarkdownProps = {
   children: string
 }
 
 const Markdown = ({ children }: MarkdownProps) => {
-  return (
-    <ReactMarkdown
-      children={children}
-      components={{ a: MarkdownLink}}
-    />
-  )
+  return <ReactMarkdown children={children} components={{ a: MarkdownLink }} />
 }
 
 export default Markdown
