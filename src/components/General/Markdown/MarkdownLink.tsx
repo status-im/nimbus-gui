ecd0cc26534c413a843fdf53dd977b2ff85f5a73
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-26 08:49:56 +0300

    feat: add style to markdown link

--- src/components/General/Markdown/MarkdownLink.tsx
+++ src/components/General/Markdown/MarkdownLink.tsx
@@ -2,7 +2,11 @@ import { Link } from 'react-router-dom'
 
 const MarkdownLink = (props: any) => {
   return (
-    <Link to={props.href} target="_blank">
+    <Link
+      to={props.href}
+      target="_blank"
+      style={{ color: '#647084', textDecoration: 'underline' }}
+    >
       {props.children}
     </Link>
   )
