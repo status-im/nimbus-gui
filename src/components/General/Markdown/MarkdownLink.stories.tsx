4dfff15c8981a1d2ef5187d7416d9d154e1570ff
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-26 08:47:45 +0300

    feat: create story for markdown link

---
+++ src/components/General/Markdown/MarkdownLink.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+import MarkdownLink from './MarkdownLink'
+
+const meta = {
+  title: 'General/MarkdownLink',
+  component: MarkdownLink,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [withRouter()],
+} satisfies Meta<typeof MarkdownLink>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    href: '/',
+    children: 'MarkdownLink',
+  },
+}
