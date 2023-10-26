d81fd7df47db0cc9e7348df2f7758b95698c0a57
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-26 09:21:26 +0300

    fix: remove markdown text

--- src/components/General/Markdown/Markdown.tsx
+++ src/components/General/Markdown/Markdown.tsx
@@ -2,13 +2,19 @@ import ReactMarkdown from 'react-markdown'
 
 import MarkdownLink from './MarkdownLink'
 import MarkdownText from './MarkdownText'
+import MarkdownCode from './MarkdownCode'
 
 type MarkdownProps = {
   children: string
 }
 
 const Markdown = ({ children }: MarkdownProps) => {
-  return <ReactMarkdown children={children} components={{ a: MarkdownLink, p: MarkdownText }} />
+  return (
+    <ReactMarkdown
+      children={children}
+      components={{ a: MarkdownLink}}
+    />
+  )
 }
 
 export default Markdown
