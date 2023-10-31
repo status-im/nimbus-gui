3ab086edcd8541666ff0a3b8608211c5263c947d
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-10-31 18:05:39 +0200

    feat: create story for keystore backups card

---
+++ src/pages/ValidatorOnboarding/KeyGeneration/KeystoreFiles/KeystoreBackupsCard.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import KeystoreBackupsCard from './KeystoreBackupsCard'
+
+const meta = {
+  title: 'ValidatorOnboarding/KeystoreBackupsCard',
+  component: KeystoreBackupsCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof KeystoreBackupsCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
