c60ca53fd4f2301fcc99f455b87c32bb832607b7
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-02 23:06:34 +0200

    feat: create base component for content

--- src/pages/ValidatorManagement/ValidatorManagement.tsx
+++ src/pages/ValidatorManagement/ValidatorManagement.tsx
@@ -2,11 +2,13 @@ import { XStack } from 'tamagui'
 
 import RightSidebar from '../Dashboard/RightSideBar/RightSidebar'
 import LeftSidebar from '../Dashboard/LeftSidebar'
+import ValidatorManagementContent from './ValidatorManagementContent'
 
 const ValidatorManagement = () => {
   return (
     <XStack style={{ height: '100vh' }}>
       <LeftSidebar />
+      <ValidatorManagementContent />
       <RightSidebar />
     </XStack>
   )
---
+++ src/pages/ValidatorManagement/ValidatorManagementContent.tsx
@@ -0,0 +1,7 @@
+import { YStack } from 'tamagui'
+
+const ValidatorManagementContent = () => {
+  return <YStack></YStack>
+}
+
+export default ValidatorManagementContent
