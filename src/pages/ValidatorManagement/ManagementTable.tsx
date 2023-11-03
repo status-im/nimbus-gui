bd8fe67c99fd9439f3be6efffa96d31d3cb4d385
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-03 10:02:46 +0200

    feat: create table structure

--- src/pages/ValidatorManagement/ManagementTable.tsx
+++ src/pages/ValidatorManagement/ManagementTable.tsx
@@ -1,5 +1,40 @@
+import './ManagementTable.css'
+
 const ManagementTable = () => {
-  return <table></table>
+  const validators = []
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Balance</th>
+          <th>Income</th>
+          <th>Proposals</th>
+          <th>Attestations</th>
+          <th>Effectiveness</th>
+          <th>Deposits</th>
+          <th>Rank</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {validators.map((validator, index) => (
+          <tr key={index}>
+            <td></td>
+            <td>{validator.balance}</td>
+            <td>{validator.income}</td>
+            <td>{validator.proposals}</td>
+            <td>{validator.attestations}</td>
+            <td>{validator.effectiveness}</td>
+            <td>{validator.deposits}</td>
+            <td>{validator.rank}</td>
+            <td>{validator.status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default ManagementTable
