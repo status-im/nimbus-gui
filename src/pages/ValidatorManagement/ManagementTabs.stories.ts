7c7582c27146fa99fd1bb613a76b54d92b33196f
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-08 00:06:48 +0200

    feat: create management tabs story

---
+++ src/pages/ValidatorManagement/ManagementTabs.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ManagementTabs from './ManagementTabs'
+
+const meta = {
+  title: 'ValidatorManagement/ManagementTabs',
+  component: ManagementTabs,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ManagementTabs>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Page: Story = {
+  args: {},
+}
