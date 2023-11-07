bf12cc552fec9d22b18d652b7f6d01f07e307ca8
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-08 00:05:40 +0200

    feat: create management tabs

---
+++ src/pages/ValidatorManagement/ManagementTabs.tsx
@@ -0,0 +1,38 @@
+import { useMemo } from 'react'
+import { Tabs } from '@status-im/components'
+import { Stack } from 'tamagui'
+
+import ManagementTable from './ManagementTable/ManagementTable'
+
+const ManagementTabs = () => {
+  const VALIDATOR_TABS = useMemo(
+    () => [
+      {
+        label: 'Active',
+        value: 'active',
+      },
+    ],
+    [],
+  )
+
+  return (
+    <Tabs defaultValue="active">
+      <Stack style={{ cursor: 'pointer', width: 'fit-content' }}>
+        <Tabs.List size={32}>
+          {VALIDATOR_TABS.map(tab => (
+            <Tabs.Trigger key={tab.value} type="default" value={tab.value}>
+              {tab.label}
+            </Tabs.Trigger>
+          ))}
+        </Tabs.List>
+      </Stack>
+      {VALIDATOR_TABS.map(tab => (
+        <Tabs.Content key={tab.value} value={tab.value} style={{ marginTop: '8px' }}>
+          <ManagementTable />
+        </Tabs.Content>
+      ))}
+    </Tabs>
+  )
+}
+
+export default ManagementTabs
