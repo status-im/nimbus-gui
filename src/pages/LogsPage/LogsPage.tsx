f609e58d70509c3c2a7ce873f177dd410efd5027
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-11-07 02:06:00 +0200

    create LogsPage

--- src/pages/LogsPage/LogsPage.tsx
+++ src/pages/LogsPage/LogsPage.tsx
@@ -1,27 +1,43 @@
-import { Stack, YStack } from "tamagui";
+import { Stack, XStack, YStack } from "tamagui";
 import TitleLogo from "../Dashboard/TitleLogo";
-import LogsTerminal from "./logsTerminal";
+
+import LeftSidebar from "../../components/General/LeftSidebar/LeftSidebar";
+import RightSidebar from "../../components/General/RightSideBar/RightSidebar";
+import LogsTerminal from "./LogsTerminal";
+import LogsSumCard from "./LogsSumCard";
 
 
 
 const LogsPage = () => {
     return (
-        <YStack
-            space="$4"
-            alignItems="start"
-            px="24px"
-            minWidth="500px"
-            width="50vh"
-            style={{
-                flexGrow: '1',
-                marginTop: '16px',
-                overflowY: 'auto',
-            }}
-            className={'transparent-scrollbar'}
-        >
-            <TitleLogo />
-            <LogsTerminal></LogsTerminal>
-        </YStack>
+        <XStack style={{ height: '100vh' }}>
+            <LeftSidebar />
+            <YStack
+                space="$4"
+                alignItems="start"
+                px="24px"
+                minWidth="500px"
+                width="50vh"
+                style={{
+                    flexGrow: '1',
+                    marginTop: '16px',
+                    overflowY: 'auto',
+                }}
+                className={'transparent-scrollbar'}
+            >
+                <TitleLogo />
+                <Stack style={{ width: '100%', alignItems: 'center' }}>
+                    <LogsTerminal />
+                    <XStack space='$8'>
+                        <LogsSumCard />
+                        <LogsSumCard />
+                        <LogsSumCard />
+                        <LogsSumCard />
+                    </XStack>
+                </Stack>
+            </YStack>
+            <RightSidebar />
+        </XStack >
     )
 }
 
