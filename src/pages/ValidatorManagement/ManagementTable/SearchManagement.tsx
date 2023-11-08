d9ce02ee18de3673b8d9c122634426048ef52296
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-08 22:21:37 +0200

    feat: create component for search validators

---
+++ src/pages/ValidatorManagement/ManagementTable/SearchManagement.tsx
@@ -0,0 +1,22 @@
+import { Input } from '@status-im/components'
+import { SearchIcon } from '@status-im/icons'
+
+type SearchManagementProps = {
+  searchValue: string
+  changeSearchValue: (value: string) => void
+}
+
+const SearchManagement = ({ searchValue, changeSearchValue }: SearchManagementProps) => {
+  return (
+    <Input
+      placeholder="Filter Validators"
+      value={searchValue}
+      onChangeText={changeSearchValue}
+      icon={<SearchIcon size={20} />}
+      onClear={() => changeSearchValue('')}
+      size={40}
+    />
+  )
+}
+
+export default SearchManagement
