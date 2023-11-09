5dd33ca2a15b61e71ba44362dbac34ca95774ed7
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-09 21:53:14 +0200

    feat: add sync cards to header

--- src/pages/ValidatorManagement/ManagementHeader.tsx
+++ src/pages/ValidatorManagement/ManagementHeader.tsx
@@ -1,11 +1,26 @@
 import { XStack } from 'tamagui'
 
 import TitleLogo from '../../components/General/TitleLogo'
+import SyncStatusCard from '../../components/General/SyncStatusCard'
 
 const ManagementHeader = () => {
   return (
-    <XStack style={{ width: '100%', justifyContent: 'space-between' }}>
+    <XStack style={{ width: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
       <TitleLogo subtitle="Validator Management" />
+      <XStack space={'$2'}>
+        <SyncStatusCard
+          synced={123.524}
+          total={172.503}
+          title="Execution Sync Status"
+          color="#2a4af5"
+        />
+        <SyncStatusCard
+          synced={123.524}
+          total={172.503}
+          title="Consensus Sync Status"
+          color="#ff6161"
+        />
+      </XStack>
     </XStack>
   )
 }
