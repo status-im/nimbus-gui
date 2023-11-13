a1210e2832c56d48da6644591e8c2dc0ca7552ed
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-14 00:02:04 +0200

    feat: create management card structure and style

--- src/pages/ValidatorManagement/ManagementCard.tsx
+++ src/pages/ValidatorManagement/ManagementCard.tsx
@@ -1,7 +1,31 @@
-import { XStack } from 'tamagui'
+import { Text } from '@status-im/components'
+import { Separator, Stack, YStack } from 'tamagui'
 
 const ManagementCard = () => {
-  return <XStack style={{ height: '100vh' }}></XStack>
+  return (
+    <YStack
+      space={'$3'}
+      style={{ border: '1px solid #F0F2F5', borderRadius: '16px', width: '297px' }}
+    >
+      <Stack style={{ padding: '12px 16px' }}>
+        <Text size={15} weight={'semibold'}>
+          Validators
+        </Text>
+      </Stack>
+      <Separator borderColor={'#F0F2F5'} />
+      <Stack style={{ padding: '12px 16px' }}>
+        <Text size={15} weight={'semibold'} color="#647084">
+          Total Balance
+        </Text>
+      </Stack>
+      <Separator borderColor={'#F0F2F5'} />
+      <Stack style={{ padding: '12px 16px', marginBottom: '16px' }}>
+        <Text size={15} weight={'semibold'} color="#647084">
+          Total Income
+        </Text>
+      </Stack>
+    </YStack>
+  )
 }
 
 export default ManagementCard
