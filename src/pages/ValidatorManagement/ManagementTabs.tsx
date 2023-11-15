41c1079767fb8e65febb3b1102c0270d90d38350
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-16 00:10:32 +0200

    fix: add div parent to tabs for width

--- src/pages/ValidatorManagement/ManagementTabs.tsx
+++ src/pages/ValidatorManagement/ManagementTabs.tsx
@@ -6,22 +6,24 @@ import { VALIDATOR_TABS } from '../../constants'
 
 const ManagementTabs = () => {
   return (
-    <Tabs defaultValue={VALIDATOR_TABS[0]}>
-      <Stack style={{ cursor: 'pointer', width: 'fit-content', margin: '16px 0' }}>
-        <Tabs.List size={32}>
-          {VALIDATOR_TABS.map(tab => (
-            <Tabs.Trigger key={tab} type="default" value={tab}>
-              {tab}
-            </Tabs.Trigger>
-          ))}
-        </Tabs.List>
-      </Stack>
-      {VALIDATOR_TABS.map(tab => (
-        <Tabs.Content key={tab} value={tab} style={{ marginTop: '8px' }}>
-          <ManagementTable tab={tab} />
-        </Tabs.Content>
-      ))}
-    </Tabs>
+    <div style={{ width: '100%' }}>
+      <Tabs defaultValue={VALIDATOR_TABS[0]}>
+        <Stack style={{ cursor: 'pointer', width: 'fit-content', margin: '16px 0' }}>
+          <Tabs.List size={32}>
+            {VALIDATOR_TABS.map(tab => (
+              <Tabs.Trigger key={tab} type="default" value={tab}>
+                {tab}
+              </Tabs.Trigger>
+            ))}
+          </Tabs.List>
+        </Stack>
+        {VALIDATOR_TABS.map(tab => (
+          <Tabs.Content key={tab} value={tab} style={{ marginTop: '8px' }}>
+            <ManagementTable tab={tab} />
+          </Tabs.Content>
+        ))}
+      </Tabs>
+    </div>
   )
 }
 
