0d546ddc5f0ca9a48d4beaa7c963809e8c459557
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-20 19:16:05 +0200

    feat: separate table row

--- src/pages/ValidatorManagement/ManagementTable/ManagementTableBody.tsx
+++ src/pages/ValidatorManagement/ManagementTable/ManagementTableBody.tsx
@@ -1,8 +1,7 @@
-import { Checkbox, Text } from '@status-im/components'
-import { OptionsIcon } from '@status-im/icons'
+import { Text } from '@status-im/components'
 
-import ValidatorProfile from '../../../components/General/ValidatorProfile'
 import { Validator } from './ManagementTable'
+import ManagementTableRow from './ManagementTableRow'
 
 type ManagementTableBodyProps = {
   filteredValidators: Validator[]
@@ -11,48 +10,8 @@ type ManagementTableBodyProps = {
 const ManagementTableBody = ({ filteredValidators }: ManagementTableBodyProps) => {
   return (
     <tbody>
-      {filteredValidators.map((validator, index) => (
-        <tr key={index}>
-          <td>
-            <Checkbox id={index.toString()} variant="outline" />
-          </td>
-          <td>
-            <ValidatorProfile number={validator.number} address={validator.address} />
-          </td>
-          <td>
-            <Text size={15} color={'#647084'} weight={'semibold'}>
-              {validator.balance}
-            </Text>
-          </td>
-          <td>
-            <Text size={15} color={'#647084'} weight={'semibold'}>
-              {validator.income}
-            </Text>
-          </td>
-          <td>
-            <Text size={15} color={'#647084'}>
-              {validator.proposals}
-            </Text>
-          </td>
-          <td>
-            <Text size={15} color={'#647084'}>
-              {validator.attestations}
-            </Text>
-          </td>
-          <td>
-            <Text size={15} color={'#647084'}>
-              {validator.effectiveness}%
-            </Text>
-          </td>
-          <td>
-            <Text size={15} color={'#2F80ED'} weight={'semibold'}>
-              {validator.status}
-            </Text>
-          </td>
-          <td>
-            <OptionsIcon size={20} color="#647084" style={{ cursor: 'pointer' }} />
-          </td>
-        </tr>
+      {filteredValidators.map(validator => (
+        <ManagementTableRow key={validator.address} validator={validator} />
       ))}
       {filteredValidators.length === 0 && (
         <tr>
---
+++ src/pages/ValidatorManagement/ManagementTable/ManagementTableRow.tsx
@@ -0,0 +1,57 @@
+import { Checkbox, Text } from '@status-im/components'
+import { OptionsIcon } from '@status-im/icons'
+
+import ValidatorProfile from '../../../components/General/ValidatorProfile'
+import { Validator } from './ManagementTable'
+
+type ManagementTableRowProps = {
+  validator: Validator
+}
+
+const ManagementTableRow = ({ validator }: ManagementTableRowProps) => {
+  return (
+    <tr>
+      <td>
+        <Checkbox id={validator.address} variant="outline" />
+      </td>
+      <td>
+        <ValidatorProfile number={validator.number} address={validator.address} />
+      </td>
+      <td>
+        <Text size={15} color={'#647084'} weight={'semibold'}>
+          {validator.balance}
+        </Text>
+      </td>
+      <td>
+        <Text size={15} color={'#647084'} weight={'semibold'}>
+          {validator.income}
+        </Text>
+      </td>
+      <td>
+        <Text size={15} color={'#647084'}>
+          {validator.proposals}
+        </Text>
+      </td>
+      <td>
+        <Text size={15} color={'#647084'}>
+          {validator.attestations}
+        </Text>
+      </td>
+      <td>
+        <Text size={15} color={'#647084'}>
+          {validator.effectiveness}%
+        </Text>
+      </td>
+      <td>
+        <Text size={15} color={'#2F80ED'} weight={'semibold'}>
+          {validator.status}
+        </Text>
+      </td>
+      <td>
+        <OptionsIcon size={20} color="#647084" style={{ cursor: 'pointer' }} />
+      </td>
+    </tr>
+  )
+}
+
+export default ManagementTableRow
