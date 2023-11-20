5a567378091d1788638a948b7b62a546f079e426
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-11-20 18:49:05 +0200

    Update SwitchComponent.tsx

--- src/pages/LogsPage/SwitchComponent.tsx
+++ src/pages/LogsPage/SwitchComponent.tsx
@@ -1,20 +1,27 @@
-import { Switch } from "tamagui"
+import { Switch } from 'tamagui'
+type SwitchComponentProps = {
+  timestamps: boolean
+  setTimestamps: (timestamps: boolean) => void
+}
 
-const SwitchComponent = () => {
+const SwitchComponent = ({ timestamps, setTimestamps }: SwitchComponentProps) => {
+  const handleCheckedChange = () => {
+    setTimestamps(!timestamps)
+  }
   return (
     <Switch
       size="$1"
-      style={ { backgroundColor: 'grey'} }
-      checked={false}
-      onCheckedChange={() =>(console.log(a))}
+      style={{ backgroundColor: timestamps ? '#4360df' : 'grey', border: 'none' }}
+      checked={timestamps}
+      onCheckedChange={handleCheckedChange}
     >
       <Switch.Thumb
         style={{
           right: 7,
           bottom: 3,
           backgroundColor: '#fff',
-          height: '16px',
-          width: '16px',
+          height: '18px',
+          width: '18px',
         }}
       />
     </Switch>
