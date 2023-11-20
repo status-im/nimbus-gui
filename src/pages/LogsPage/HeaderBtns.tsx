8da7de9a3a057e4a589abb5006db15751f4c1b61
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2023-11-20 18:58:20 +0200

    Create HeaderBtns.tsx

---
+++ src/pages/LogsPage/HeaderBtns.tsx
@@ -0,0 +1,48 @@
+import { Button, DropdownMenu, Input } from '@status-im/components'
+import { Stack, XStack } from 'tamagui'
+import { SearchIcon, DropdownIcon } from '@status-im/icons'
+
+import SwitchComponent from './SwitchComponent'
+
+type HeaderBtnsProps = {
+  setDropdownMenuItem: (dropdownMenuItem: string) => void
+  dropdownMenuItem: string
+  timestamps: boolean
+  setTimestamps: (timestamps: boolean) => void
+}
+
+const HeaderBtns = ({
+  setDropdownMenuItem,
+  dropdownMenuItem,
+  timestamps,
+  setTimestamps,
+}: HeaderBtnsProps) => {
+  return (
+    <XStack space={'$4'}>
+      <Input variant="retractable" color={'black'} icon={<SearchIcon size={16} />}></Input>
+      <Stack>
+        <DropdownMenu>
+          <Button variant="outline" icon={<DropdownIcon size={16} />}>
+            {dropdownMenuItem}
+          </Button>
+          <DropdownMenu.Content>
+            <DropdownMenu.Item
+              label="Validator"
+              onSelect={() => setDropdownMenuItem('Validator')}
+            />
+            <DropdownMenu.Item label="Critical" onSelect={() => setDropdownMenuItem('Critical')} />
+            <DropdownMenu.Item label="Warnings" onSelect={() => setDropdownMenuItem('Warnings')} />
+          </DropdownMenu.Content>
+        </DropdownMenu>
+      </Stack>
+      <Button
+        variant="outline"
+        icon={<SwitchComponent timestamps={timestamps} setTimestamps={setTimestamps} />}
+      >
+        Timestamps
+      </Button>
+    </XStack>
+  )
+}
+
+export default HeaderBtns
