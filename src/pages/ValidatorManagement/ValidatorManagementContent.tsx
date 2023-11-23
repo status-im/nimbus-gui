54715ca0a31707835cf5757a372f5e1133e2e389
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-11-23 09:26:04 +0200

    fix: remove width and min width management container

--- src/pages/ValidatorManagement/ValidatorManagementContent.tsx
+++ src/pages/ValidatorManagement/ValidatorManagementContent.tsx
@@ -12,8 +12,6 @@ const ValidatorManagementContent = () => {
       space="$4"
       alignItems="start"
       px="24px"
-      minWidth="500px"
-      width="50vh"
       style={{
         flexGrow: '1',
         overflowY: 'auto',
