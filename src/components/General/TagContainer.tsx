b8ad2b0a3fff581fe77e7f8b24e9ac4f26bea003
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2023-12-20 10:47:17 +0200

    fix: remove connect device tab

--- src/components/General/TagContainer.tsx
+++ src/components/General/TagContainer.tsx
@@ -1,7 +1,7 @@
 import { Tag } from '@status-im/components'
 import { XStack } from 'tamagui'
 import './TagContainer.css'
-import { ConnectionIcon, AddSmallIcon, SwapIcon } from '@status-im/icons'
+import { AddSmallIcon, SwapIcon } from '@status-im/icons'
 import { useNavigate } from 'react-router'
 
 type TagContainerProps = {
@@ -10,10 +10,6 @@ type TagContainerProps = {
 
 const TagContainer = ({ selectedTag }: TagContainerProps) => {
   const navigate = useNavigate()
-
-  const onPressConnect = () => {
-    navigate('/connect-device')
-  }
 
   const onPressPair = () => {
     navigate('/pair-device')
@@ -25,15 +21,6 @@ const TagContainer = ({ selectedTag }: TagContainerProps) => {
 
   return (
     <XStack space={'$2'} alignItems="center" className="tag-container">
-      {selectedTag === 'connect' ? (
-        <Tag
-          selected={selectedTag === 'connect'}
-          icon={ConnectionIcon}
-          label="Connect"
-          size={32}
-          onPress={onPressConnect}
-        />
-      ) : null}
       <Tag
         selected={selectedTag === 'pair'}
         icon={SwapIcon}
