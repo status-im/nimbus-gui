75e6c5c4103ad01a2af130f181f84182c5dd1bc0
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-01-19 09:50:57 +0200

    fix: rewrite style of overview to module

--- src/pages/ValidatorOnboarding/Overview/OverviewWrapper.tsx
+++ src/pages/ValidatorOnboarding/Overview/OverviewWrapper.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 import { useTheme } from 'tamagui'
 
-import './validatorLayout.css'
+import styles from './validatorLayout.module.css'
 
 type OverviewWrapperProps = {
   rightImageSrc?: string
@@ -18,18 +18,18 @@ const OverviewWrapper = ({
   const theme = useTheme()
 
   return (
-    <div className="layout" style={{ backgroundColor: theme.background.val }}>
-      <section className="layout-left">
-        <div className="container">
-          <div className="container-inner">{children}</div>
+    <div className={styles['layout']} style={{ backgroundColor: theme.background.val }}>
+      <section className={styles['layout-left']}>
+        <div className={styles['container']}>
+          <div className={styles['container-inner']}>{children}</div>
         </div>
       </section>
-      <section className="layout-right">
-        <div className="image-container">
+      <section className={styles['layout-right']}>
+        <div className={styles['image-container']}>
           <img
             src={rightImageSrc}
             alt="background"
-            className="background-img"
+            className={styles['background-img']}
             style={{ height: imgHeight }}
           />
         </div>
