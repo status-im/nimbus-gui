c9aacf51fa7e46cd6661801c7253b2e3e4467a3c
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-01-27 09:22:05 +0200

    fix: add space between cards in column

--- src/pages/ValidatorOnboarding/Activation/ActivationCard.tsx
+++ src/pages/ValidatorOnboarding/Activation/ActivationCard.tsx
@@ -9,6 +9,7 @@ type ActivationCardProps = {
   gaugeColor?: string
   gaugeSynced?: number
   gaugeTotal?: number
+  isLast?: boolean
 }
 
 const ActivationCard = ({
@@ -18,6 +19,7 @@ const ActivationCard = ({
   gaugeColor,
   gaugeSynced,
   gaugeTotal,
+  isLast,
 }: ActivationCardProps) => {
   return (
     <YStack
@@ -26,7 +28,7 @@ const ActivationCard = ({
         border: '1px solid rgba(0, 0, 0, 0.15)',
         padding: '12px 16px',
         backgroundColor: '#FFF',
-
+        marginBottom: !isLast && '10px',
         flex: 1,
       }}
     >
