573eeae2d979ce03c699de4af4f80bdca304e1f1
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-01-29 13:09:04 +0200

    Use Outlet component to get children componnets

--- src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
+++ src/pages/ValidatorOnboarding/ValidatorOnboarding.tsx
@@ -18,6 +18,7 @@ import ActivationValidatorSetup from './ValidatorSetup/ValidatorActivation/Activ
 import Deposit from './Deposit/Deposit'
 import { useWindowSize } from '../../hooks/useWindowSize'
 import styles from './layoutGradient.module.css'
+import { Outlet } from 'react-router-dom';
 
 const ValidatorOnboarding = () => {
   const { activeStep, subStepValidatorSetup } = useSelector(
@@ -42,37 +43,7 @@ const ValidatorOnboarding = () => {
         />
         <FormStepper activeStep={activeStep} />
         <ValidatorBoxWrapper>
-          {activeStep === 0 && <Overview />}
-          {activeStep === 1 && <Advisories />}
-          {activeStep === 2 && subStepValidatorSetup === 0 && <ValidatorSetup />}
-          {activeStep === 2 && subStepValidatorSetup === 1 && <ValidatorSetupInstall />}
-          {activeStep === 2 && subStepValidatorSetup === 2 && <ConsensusSelection />}
-          {activeStep === 2 && subStepValidatorSetup === 3 && <ActivationValidatorSetup />}
-          {activeStep === 3 && <ClientSetup />}
-          {activeStep === 4 && <KeyGeneration />}
-          {activeStep === 5 && <Deposit />}
-          {activeStep === 6 && (
-            <Activation
-              validatorsValue="4"
-              executionSyncStatus1={{
-                text: 'Execution Sync Status',
-                isGaugeIncluded: true,
-                gaugeColor: '$blue',
-                gaugeSynced: 123.524,
-                gaugeTotal: 172.503,
-              }}
-              executionSyncStatus2={{
-                text: 'Execution Sync Status',
-                isGaugeIncluded: true,
-                gaugeColor: '$red',
-                gaugeSynced: 123.524,
-                gaugeTotal: 172.503,
-              }}
-              currentAPRValue="4.40%"
-              estimatedActivationTimeValue="32 Days"
-              validatorQueueValue="92603"
-            />
-          )}
+          <Outlet />  
         </ValidatorBoxWrapper>
         <ContinueButton />
       </YStack>
