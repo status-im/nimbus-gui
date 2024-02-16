37f945238715d4bd497a69d835be40089701f94c
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-02-16 10:17:03 +0200

    feat(tamagui-switch): create component with style

--- src/components/General/TamaguiSwitch.tsx
+++ src/components/General/TamaguiSwitch.tsx
@@ -1,9 +1,30 @@
 import { Switch } from 'tamagui'
 
-const TamaguiSwitch = () => {
+type TamaguiSwitchProps = {
+  isSwitchOn: boolean | undefined
+  onSwitchChange: (value: boolean) => void
+}
+
+const TamaguiSwitch = ({ isSwitchOn, onSwitchChange }: TamaguiSwitchProps) => {
+  const switchStyle = isSwitchOn
+    ? { backgroundColor: '#2A4AF5' }
+    : { backgroundColor: 'grey' }
+
   return (
-    <Switch size="$4">
-      <Switch.Thumb />
+    <Switch
+      size="$2"
+      style={switchStyle}
+      checked={isSwitchOn}
+      onCheckedChange={onSwitchChange}
+      height={24}
+      width={36}
+      cursor="pointer"
+    >
+      {isSwitchOn ? (
+        <Switch.Thumb width={16} height={16} bottom={-2} right={2} />
+      ) : (
+        <Switch.Thumb width={16} height={16} bottom={-2} left={2} />
+      )}
     </Switch>
   )
 }
