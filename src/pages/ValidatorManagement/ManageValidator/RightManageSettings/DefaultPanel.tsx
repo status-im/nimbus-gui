7e5d73e4f9741979cbfda7d3c6a5ab5ad0898a51
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-02-19 21:43:23 +0200

    feat(default-panel): create and use separate component

---
+++ src/pages/ValidatorManagement/ManageValidator/RightManageSettings/DefaultPanel.tsx
@@ -0,0 +1,25 @@
+import { Text } from '@status-im/components'
+import { YStack } from 'tamagui'
+
+import Icon from '../../../../components/General/Icon'
+
+const DefaultPanel = () => {
+  return (
+    <YStack
+      space={'$2'}
+      width={'50%'}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Icon src="/icons/cat-in-box.png" height={100} width={100} />
+      <Text size={15} weight={'semibold'}>
+        Manage your Validator
+      </Text>
+      <Text size={15}>
+        This panel will give you additional settings to manage your vaidator
+      </Text>
+    </YStack>
+  )
+}
+
+export default DefaultPanel
--- src/pages/ValidatorManagement/ManageValidator/RightManageSettings/RightManageSettings.tsx
+++ src/pages/ValidatorManagement/ManageValidator/RightManageSettings/RightManageSettings.tsx
@@ -1,29 +1,11 @@
-import { Text } from '@status-im/components'
-import { YStack } from 'tamagui'
-
-import Icon from '../../../../components/General/Icon'
+import DefaultPanel from './DefaultPanel'
 
 const RightManageSettings = () => {
   const isDefault = false
-  const isMigrate = true
-  const isDeposit = false
+  // const isMigrate = true
+  // const isDeposit = false
 
-  return (
-    <YStack
-      space={'$2'}
-      width={'50%'}
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Icon src="/icons/cat-in-box.png" height={100} width={100} />
-      <Text size={15} weight={'semibold'}>
-        Manage your Validator
-      </Text>
-      <Text size={15}>
-        This panel will give you additional settings to manage your vaidator
-      </Text>
-    </YStack>
-  )
+  return <>{isDefault && <DefaultPanel />}</>
 }
 
 export default RightManageSettings
