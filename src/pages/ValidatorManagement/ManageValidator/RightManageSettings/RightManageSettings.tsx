aaa6b4c659326c044e5bb48007fee3b0cda78fae
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-02-19 21:56:09 +0200

    feat(right-manage-settings): use new components

--- src/pages/ValidatorManagement/ManageValidator/RightManageSettings/RightManageSettings.tsx
+++ src/pages/ValidatorManagement/ManageValidator/RightManageSettings/RightManageSettings.tsx
@@ -1,11 +1,19 @@
 import DefaultPanel from './Panels/DefaultPanel'
+import DepositPanel from './Panels/DepositPanel'
+import MigratePanel from './Panels/MigratePanel'
 
 const RightManageSettings = () => {
   const isDefault = false
-  // const isMigrate = true
-  // const isDeposit = false
+  const isMigrate = true
+  const isDeposit = false
 
-  return <>{isDefault && <DefaultPanel />}</>
+  return (
+    <>
+      {isDefault && <DefaultPanel />}
+      {isMigrate && <MigratePanel />}
+      {isDeposit && <DepositPanel />}
+    </>
+  )
 }
 
 export default RightManageSettings
