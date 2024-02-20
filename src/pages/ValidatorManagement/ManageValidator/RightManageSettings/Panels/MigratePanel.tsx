855c00412e1b18dbbb1874fe455224b25be08077
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-02-20 10:26:53 +0200

    feat(migrate-panel): add rest of the content components

--- src/pages/ValidatorManagement/ManageValidator/RightManageSettings/Panels/MigratePanel.tsx
+++ src/pages/ValidatorManagement/ManageValidator/RightManageSettings/Panels/MigratePanel.tsx
@@ -1,22 +1,28 @@
-import { InformationBox, Text } from '@status-im/components'
+import { Button, InformationBox, Text } from '@status-im/components'
 import { CloseCircleIcon } from '@status-im/icons'
 import { YStack } from 'tamagui'
 
+import KeystoreBackupsCard from '../../../../ValidatorOnboarding/KeyGeneration/KeystoreFiles/KeystoreBackupsCard'
+
 const MigratePanel = () => {
+  const migrateValidatorHandler = () => {}
   return (
     <YStack>
       <Text size={19} weight={'semibold'}>
         Migrate Validator
       </Text>
+      <img src="" alt="" />
       <Text size={15}>
         You are about to Migrate your validator it is not possible for you to
         revert this action! Please make sure you understand the consequences.
       </Text>
+      <KeystoreBackupsCard />
       <InformationBox
         message="Please make sure to enforce the file on another node to continue validating with your Validator."
         variant="error"
         icon={<CloseCircleIcon size={20} />}
       />
+      <Button onPress={migrateValidatorHandler}>Migrate Validator</Button>
     </YStack>
   )
 }
