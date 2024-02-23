2cfaf4cdd70a6c01f75fcc15b54a2371344f2f73
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-02-23 08:20:09 +0200

    feat(deposit-panel): add base

--- src/pages/ValidatorManagement/ManageValidator/RightManageSettings/Panels/DepositPanel.tsx
+++ src/pages/ValidatorManagement/ManageValidator/RightManageSettings/Panels/DepositPanel.tsx
@@ -1,7 +1,32 @@
-import Deposit from '../../../../ValidatorOnboarding/Deposit/Deposit'
+import { Button, Text } from '@status-im/components'
+import { XStack, YStack } from 'tamagui'
+
+import PanelWrapper from './PanelWrapper'
 
 const DepositPanel = () => {
-  return <Deposit />
+  const connectWalletHandler = () => {}
+
+  return (
+    <PanelWrapper>
+      <Text size={19} weight={'semibold'}>
+        Deposit Funds
+      </Text>
+      <YStack
+        space={'$3'}
+        style={{
+          alignItems: 'center',
+          textAlign: 'center',
+          marginBottom: '6px',
+        }}
+      >
+        <XStack
+          style={{ width: '100%', justifyContent: 'end', marginTop: '6px' }}
+        >
+          <Button onPress={connectWalletHandler}>Connect Wallet</Button>
+        </XStack>
+      </YStack>
+    </PanelWrapper>
+  )
 }
 
 export default DepositPanel
