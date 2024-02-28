e8bb940d6cf169cb017216e954e369745cd79350
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-02-28 23:59:41 +0200

    feat(currency-dropdown): show currency

--- src/components/General/CurrencyDropdown.tsx
+++ src/components/General/CurrencyDropdown.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
-import { Button, DropdownMenu } from '@status-im/components'
-import { useDispatch } from 'react-redux'
+import { Button, DropdownMenu, Text } from '@status-im/components'
+import { useDispatch, useSelector } from 'react-redux'
+import { XStack, YStack } from 'tamagui'
 
 import { CURRENCIES } from '../../constants'
 import ChevronIcon from './ChevronIcon'
+import { RootState } from '../../redux/store'
 
-const CurrencyDropdown = () => {
+type CurrencyDropdownProps = {
+  totalPrice: number
+}
+
+const CurrencyDropdown = ({ totalPrice }: CurrencyDropdownProps) => {
   const [isOpen, setIsOpen] = useState(false)
   const dispatch = useDispatch()
+  const currency = useSelector((state: RootState) => state.currency)
 
   const changeIsOpenHandler = (isOpen: boolean) => {
     setIsOpen(isOpen)
@@ -18,22 +25,32 @@ const CurrencyDropdown = () => {
   }
 
   return (
-    <DropdownMenu onOpenChange={changeIsOpenHandler}>
-      <Button
-        variant="ghost"
-        size={24}
-        icon={<ChevronIcon isOpen={isOpen} />}
-      />
-      <DropdownMenu.Content width={63} side="bottom" zIndex={999}>
-        {Object.keys(CURRENCIES).map(currency => (
-          <DropdownMenu.Item
-            key={currency}
-            label={currency}
-            onSelect={() => changeCurrencyHandler(currency)}
+    <YStack space={'$2'}>
+      <XStack style={{ justifyContent: 'space-between' }}>
+        <Text size={15} weight={'semibold'}>
+          {currency}
+        </Text>
+        <DropdownMenu onOpenChange={changeIsOpenHandler}>
+          <Button
+            variant="ghost"
+            size={24}
+            icon={<ChevronIcon isOpen={isOpen} />}
           />
-        ))}
-      </DropdownMenu.Content>
-    </DropdownMenu>
+          <DropdownMenu.Content width={63} side="bottom" zIndex={999}>
+            {Object.keys(CURRENCIES).map(currency => (
+              <DropdownMenu.Item
+                key={currency}
+                label={currency}
+                onSelect={() => changeCurrencyHandler(currency)}
+              />
+            ))}
+          </DropdownMenu.Content>
+        </DropdownMenu>
+      </XStack>
+      <Text size={27} weight={'semibold'}>
+        {totalPrice.toFixed(2)} {currency}
+      </Text>
+    </YStack>
   )
 }
 
