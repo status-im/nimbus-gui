ac791e7f382d0fb557e2be6c409d639cf1736761
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-03-06 15:30:21 +0200

    feat(dashboard sidebar): add popover

--- src/pages/Dashboard/DashboardSidebar/DashboardSidebar.tsx
+++ src/pages/Dashboard/DashboardSidebar/DashboardSidebar.tsx
@@ -1,6 +1,6 @@
-import { Avatar, Text } from '@status-im/components'
-import { XStack, YStack } from 'tamagui'
-import { CopyIcon } from '@status-im/icons'
+import { Avatar, Popover, Text } from '@status-im/components'
+import { Stack, XStack, YStack } from 'tamagui'
+import { AddSmallIcon, CopyIcon } from '@status-im/icons'
 
 import AlertsList from '../../../components/General/RightSideBar/AlertsList'
 import LogsList from '../../../components/General/RightSideBar/LogsList'
@@ -9,7 +9,8 @@ import { copyFunction, getFormattedWalletAddress } from '../../../utilities'
 import ValidatorsTabs from '../../../components/General/RightSideBar/ValidatorsTabs/ValidatorsTabs'
 import ValidatorsCount from '../../../components/General/RightSideBar/ValidatorsCount'
 import DashboardDiamondCard from './DashboardDiamondCard'
-import AddValidatorButton from './AddValidatorButton'
+
+import NodesList from './NodesList'
 
 const DashboardSidebar = () => {
   const onCopyWalletAddress = () => {
@@ -29,7 +30,7 @@ const DashboardSidebar = () => {
         overflowY: 'auto',
       }}
     >
-      <XStack alignItems="center" space={'$2'}>
+      <XStack alignItems="center" space={'$2'} backgroundColor={'#F5F5F5'}>
         <Avatar type="user" size={32} name="Ethereum Mainnet" />
         <YStack>
           <Text size={15} weight={'semibold'}>
@@ -50,7 +51,36 @@ const DashboardSidebar = () => {
         <DashboardDiamondCard />
       </XStack>
       <ValidatorsCount />
-      <AddValidatorButton></AddValidatorButton>
+
+      <Popover
+        side="top"
+        align="center"
+        children={[
+          <YStack
+            backgroundColor={'#757e8c'}
+            style={{
+              padding: '8px 12px',
+              borderRadius: '16px',
+              flexGrow: '1',
+              height: 'max-fit',
+            }}
+          >
+            <XStack
+              style={{ marginBottom: '8px' }}
+              alignContent="center"
+              alignItems="center"
+              justifyContent="center"
+              space="$2"
+            >
+              <AddSmallIcon size={20} color="#fff"></AddSmallIcon>
+              <Text size={19} color={'#FFF'}>
+                Add Validator
+              </Text>
+            </XStack>
+          </YStack>,
+          <NodesList></NodesList>,
+        ]}
+      ></Popover>
       <ValidatorsTabs />
       <AlertsList />
       <LogsList />
