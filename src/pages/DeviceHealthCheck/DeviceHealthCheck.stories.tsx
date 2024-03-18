69c4f23e3842c256e6b32bb75d2435dc31136c88
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-03-18 19:46:33 +0200

    refactor(stories) fix Device Health Check stories

--- src/pages/DeviceHealthCheck/DeviceHealthCheck.stories.tsx
+++ src/pages/DeviceHealthCheck/DeviceHealthCheck.stories.tsx
@@ -1,11 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import DeviceHealthCheck from './DeviceHealthCheck'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 const meta: Meta = {
   title: 'Pages/DeviceHealthCheck',
   component: DeviceHealthCheck,
-  decorators: [],
+  decorators: [withRouter],
   tags: ['autodocs'],
 }
 
