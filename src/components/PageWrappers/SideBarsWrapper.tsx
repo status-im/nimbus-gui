90bbc345ad91214d58ef5b24d021c80aea803bb8
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-03-27 09:28:35 +0200

    feat(side-bars-wrapper): add component content and logic

--- src/components/PageWrappers/SideBarsWrapper.tsx
+++ src/components/PageWrappers/SideBarsWrapper.tsx
@@ -1,5 +1,32 @@
-const SideBarsWrapper = () => {
-  return <div></div>
+import { ReactNode } from 'react'
+import { XStack } from 'tamagui'
+
+import LeftSidebar from '../General/LeftSidebar/LeftSidebar'
+import RightSidebar from '../General/RightSideBar/RightSidebar'
+import styles from './side-bars-wrapper.module.css'
+
+type SideBardsWrapperProps = {
+  children: ReactNode
+  isNotLeftSideBar?: boolean
+  isNotRightSideBar?: boolean
+}
+
+const SideBarsWrapper = ({
+  children,
+  isNotLeftSideBar,
+  isNotRightSideBar,
+}: SideBardsWrapperProps) => {
+  return (
+    <XStack style={{ height: '100vh' }}>
+      {!isNotLeftSideBar && <LeftSidebar />}
+      {children}
+      {!isNotRightSideBar && (
+        <div className={styles['right-sidebar-wrapper']}>
+          <RightSidebar />
+        </div>
+      )}
+    </XStack>
+  )
 }
 
 export default SideBarsWrapper
