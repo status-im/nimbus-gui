2636d0faba4b9163c9c0ce313437cce0ac6c6818
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-04-01 23:53:08 +0300

    feat(settings cards): create stories

---
+++ src/pages/ManageValidator/LeftManage/ValidatorSettingsCards/ValidatorSettingCard.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ValidatorSettingCard from './ValidatorSettingCard'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+const meta = {
+  title: 'Manage Validator/ValidatorSettingCard',
+  component: ValidatorSettingCard,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [withRouter],
+  tags: ['autodocs'],
+} satisfies Meta<typeof ValidatorSettingCard>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Variant1: Story = {
+  args: { title: 'Exit' },
+}
+export const Variant2: Story = {
+  args: { title: 'Migrate' },
+}
+
+export const Variant3: Story = {
+  args: { title: 'Advanced' },
+}
---
+++ src/pages/ManageValidator/LeftManage/ValidatorSettingsCards/ValidatorSettingCards.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ValidatorSettingsCards from './ValidatorSettingsCards'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+const meta = {
+  title: 'Manage Validator/ValidatorSettingsCards',
+  component: ValidatorSettingsCards,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators:[withRouter],
+  tags: ['autodocs'],
+} satisfies Meta<typeof ValidatorSettingsCards>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
