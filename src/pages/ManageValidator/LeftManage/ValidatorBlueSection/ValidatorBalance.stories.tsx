fdc85874464e761c9db89ce7e2bb050c25a06bc4
Hristo Nedelkov <hristo.nedelkow@gmail.com>
2024-04-01 23:46:31 +0300

    feat(validator blue section): create stories

---
+++ src/pages/ManageValidator/LeftManage/ValidatorBlueSection/ValidatorBalance.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ValidatorBalance from './ValidatorBalance'
+
+const meta = {
+  title: 'Manage Validator/ValidatorBalance',
+  component: ValidatorBalance,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ValidatorBalance>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
---
+++ src/pages/ManageValidator/LeftManage/ValidatorBlueSection/ValidatorInfo.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import ValidatorInfo from './ValidatorInfo'
+import { withRouter } from 'storybook-addon-react-router-v6'
+
+const meta = {
+  title: 'Manage Validator/ValidatorInfo',
+  component: ValidatorInfo,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [withRouter],
+} satisfies Meta<typeof ValidatorInfo>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
