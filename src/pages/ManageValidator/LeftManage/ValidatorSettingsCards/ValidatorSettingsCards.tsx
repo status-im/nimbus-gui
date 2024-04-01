1ec43ffbfcf27093afdeb6ffbdcb5ee929016146
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-01 11:09:56 +0300

    feat(validator-settings-cards): map menu for advanced

--- src/pages/ManageValidator/LeftManage/ValidatorSettingsCards/ValidatorSettingsCards.tsx
+++ src/pages/ManageValidator/LeftManage/ValidatorSettingsCards/ValidatorSettingsCards.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'tamagui'
 
+import AdvancedDropdownMenu from './AdvancedDropdownMenu'
 import ValidatorSettingCard from './ValidatorSettingCard'
 import { VALIDATOR_SETTINGS_CARDS_TITLES } from '../../../../constants'
 
@@ -16,9 +17,13 @@ const ValidatorSettingsCards = () => {
         paddingBottom: '15px',
       }}
     >
-      {VALIDATOR_SETTINGS_CARDS_TITLES.map(title => (
-        <ValidatorSettingCard key={title} title={title} />
-      ))}
+      {VALIDATOR_SETTINGS_CARDS_TITLES.map(title =>
+        title === VALIDATOR_SETTINGS_CARDS_TITLES[2] ? (
+          <AdvancedDropdownMenu key={title} title={title} />
+        ) : (
+          <ValidatorSettingCard key={title} title={title} />
+        ),
+      )}
     </Stack>
   )
 }
