682fadf273f1d09eabb5b5823f66f669e838dc85
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-10 17:35:44 +0300

    fix(left-sidebar-icon-button-story): remove old props

--- src/components/General/LeftSidebar/LeftSidebarIconButton.stories.tsx
+++ src/components/General/LeftSidebar/LeftSidebarIconButton.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { DashboardIcon } from '@status-im/icons'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 import LeftSidebarIconButton from './LeftSidebarIconButton'
-import { withRouter } from 'storybook-addon-react-router-v6'
 
 const meta = {
   title: 'General/LeftSidebarIconButton',
@@ -21,9 +21,7 @@ export const Default: Story = {
   args: {
     iconEl: <DashboardIcon size={20} />,
     isDotOn: false,
-    isSelected: false,
     isDisabled: false,
-    id: 'dashboard',
     path: 'dashboard',
   },
 }
@@ -32,20 +30,7 @@ export const DotOn: Story = {
   args: {
     iconEl: <DashboardIcon size={20} />,
     isDotOn: true,
-    isSelected: false,
     isDisabled: false,
-    id: 'dashboard',
-    path: 'dashboard',
-  },
-}
-
-export const Selected: Story = {
-  args: {
-    iconEl: <DashboardIcon size={20} />,
-    isDotOn: false,
-    isSelected: true,
-    isDisabled: false,
-    id: 'dashboard',
     path: 'dashboard',
   },
 }
