8f112acdb0cf3359b2e243d1aba1887547eaa0ea
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-10 13:38:51 +0300

    fix(management-tabs-story): missing router decorator

--- src/pages/ValidatorManagement/ManagementTabs.stories.ts
+++ src/pages/ValidatorManagement/ManagementTabs.stories.ts
@@ -1,10 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 import ManagementTabs from './ManagementTabs'
 
 const meta = {
   title: 'ValidatorManagement/ManagementTabs',
   component: ManagementTabs,
+  decorators: [withRouter()],
   parameters: {
     layout: 'centered',
   },
