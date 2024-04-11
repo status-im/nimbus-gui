dbc834d19f81020c555acc5e2e88aba682ab5c89
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-11 15:09:02 +0300

    fix(left-sidebar-icon-button): remove static dot

--- src/components/General/LeftSidebar/LeftSidebarIconButton.tsx
+++ src/components/General/LeftSidebar/LeftSidebarIconButton.tsx
@@ -1,5 +1,4 @@
 import { IconAvatarProps, IconButton } from '@status-im/components'
-import { Stack } from 'tamagui'
 import { Link } from 'react-router-dom'
 
 type IconButtonWithDotProps = {
@@ -8,7 +7,6 @@ type IconButtonWithDotProps = {
 }
 
 const LeftSidebarIconButton = ({ icon, path }: IconButtonWithDotProps) => {
-  const isDotOn = path === '/notifications'
   const isDisabled =
     path === '/advanced-analytics' ||
     path === '/community' ||
@@ -23,20 +21,6 @@ const LeftSidebarIconButton = ({ icon, path }: IconButtonWithDotProps) => {
         selected={path === window.location.pathname}
         disabled={isDisabled}
       />
-      {isDotOn && (
-        <Stack
-          style={{
-            position: 'absolute',
-            right: 7,
-            top: 5,
-            width: '9px',
-            height: '9px',
-            borderRadius: '50%',
-            backgroundColor: '#1992D7',
-            border: '1.5px solid #fff',
-          }}
-        />
-      )}
     </Link>
   )
 }
