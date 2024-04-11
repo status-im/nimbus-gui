402f120d59e4d029349735b0fa3d43837b7d8149
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-11 16:10:45 +0300

    feat(dropdown-filter-management): use notification badge

--- src/pages/ValidatorManagement/ManagementTable/DropdownFilter.tsx
+++ src/pages/ValidatorManagement/ManagementTable/DropdownFilter.tsx
@@ -2,6 +2,8 @@ import { DropdownMenu } from '@status-im/components'
 import { SortIcon } from '@status-im/icons'
 import { Stack } from 'tamagui'
 
+import NotificationBadge from '../../../components/General/NotificationBadge'
+
 const DropdownFilter = () => {
   return (
     <DropdownMenu>
@@ -18,18 +20,7 @@ const DropdownFilter = () => {
             cursor: 'pointer',
           }}
         />
-        <Stack
-          style={{
-            position: 'absolute',
-            right: -2,
-            top: -1.5,
-            width: '9px',
-            height: '9px',
-            borderRadius: '50%',
-            backgroundColor: '#1992D7',
-            border: '1.5px solid #fff',
-          }}
-        />
+        <NotificationBadge />
       </Stack>
       <DropdownMenu.Content sideOffset={5} position="absolute" zIndex={999} />
     </DropdownMenu>
