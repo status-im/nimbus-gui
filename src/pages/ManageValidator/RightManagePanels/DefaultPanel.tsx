afb43dee76e4d57c53e255d2276b2ba56f09bc79
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-16 10:19:05 +0300

    feat(default-panel): hide for smaller screens

--- src/pages/ManageValidator/RightManagePanels/DefaultPanel.tsx
+++ src/pages/ManageValidator/RightManagePanels/DefaultPanel.tsx
@@ -2,24 +2,32 @@ import { Text } from '@status-im/components'
 import { YStack } from 'tamagui'
 
 import Icon from '../../../components/General/Icon'
+import { useWindowSize } from '../../../hooks/useWindowSize'
 
 const DefaultPanel = () => {
+  const { width } = useWindowSize()
+  const isModalWidth = width < 1160
+
   return (
-    <YStack
-      space={'$2'}
-      width={'100%'}
-      height={'100vh'}
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Icon src="/icons/cat-in-box.png" height={100} width={100} />
-      <Text size={15} weight={'semibold'}>
-        Manage your Validator
-      </Text>
-      <Text size={15}>
-        This panel will give you additional settings to manage your vaidator
-      </Text>
-    </YStack>
+    <>
+      {isModalWidth === false && (
+        <YStack
+          space={'$2'}
+          width={'100%'}
+          height={'100vh'}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Icon src="/icons/cat-in-box.png" height={100} width={100} />
+          <Text size={15} weight={'semibold'}>
+            Manage your Validator
+          </Text>
+          <Text size={15}>
+            This panel will give you additional settings to manage your vaidator
+          </Text>
+        </YStack>
+      )}
+    </>
   )
 }
 
