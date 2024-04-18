5b7f6b958747e240f16baaca8de4c7973fe52cc4
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-18 16:34:10 +0300

    feat(management-header): style containers for responsive cards

--- src/pages/ValidatorManagement/ManagementHeader.tsx
+++ src/pages/ValidatorManagement/ManagementHeader.tsx
@@ -1,31 +1,44 @@
-import { XStack } from 'tamagui'
-
 import NimbusLogo from '../../components/Logos/NimbusLogo'
 import SyncStatusCard from '../../components/General/SyncStatusCard'
+import { useWindowSize } from '../../hooks/useWindowSize'
 
 const ManagementHeader = () => {
+  const windowSize = useWindowSize()
+
   return (
-    <XStack
+    <div
       style={{
         width: '100%',
         justifyContent: 'space-between',
-        alignItems: 'center',
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '8px',
       }}
     >
       <NimbusLogo subtitle="Validator Management" />
-      <SyncStatusCard
-        synced={123.524}
-        total={172.503}
-        title="Execution Sync Status"
-        color="#2a4af5"
-      />
-      <SyncStatusCard
-        synced={123.524}
-        total={172.503}
-        title="Consensus Sync Status"
-        color="#ff6161"
-      />
-    </XStack>
+      <div
+        style={{
+          display: 'flex',
+          gap: '8px',
+          flexWrap: 'wrap',
+          minWidth: windowSize.width < 790 ? '100%' : '',
+          flexDirection: windowSize.width < 550 ? 'column' : 'row',
+        }}
+      >
+        <SyncStatusCard
+          synced={123.524}
+          total={172.503}
+          title="Execution Sync Status"
+          color="#2a4af5"
+        />
+        <SyncStatusCard
+          synced={123.524}
+          total={172.503}
+          title="Consensus Sync Status"
+          color="#ff6161"
+        />
+      </div>
+    </div>
   )
 }
 
