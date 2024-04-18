4aceca03ef4e02ebc27093ed9644608fc824e1e0
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-18 15:32:45 +0300

    fix(management-header): remove sync card containers styles

--- src/pages/ValidatorManagement/ManagementHeader.tsx
+++ src/pages/ValidatorManagement/ManagementHeader.tsx
@@ -2,7 +2,6 @@ import { XStack } from 'tamagui'
 
 import NimbusLogo from '../../components/Logos/NimbusLogo'
 import SyncStatusCard from '../../components/General/SyncStatusCard'
-import styles from './validatorManagement.module.css'
 
 const ManagementHeader = () => {
   return (
@@ -14,24 +13,18 @@ const ManagementHeader = () => {
       }}
     >
       <NimbusLogo subtitle="Validator Management" />
-      <XStack space={'$2'}>
-        <div className={styles['sync-status-card-container-first']}>
-          <SyncStatusCard
-            synced={123.524}
-            total={172.503}
-            title="Execution Sync Status"
-            color="#2a4af5"
-          />
-        </div>
-        <div className={styles['sync-status-card-container-second']}>
-          <SyncStatusCard
-            synced={123.524}
-            total={172.503}
-            title="Consensus Sync Status"
-            color="#ff6161"
-          />
-        </div>
-      </XStack>
+      <SyncStatusCard
+        synced={123.524}
+        total={172.503}
+        title="Execution Sync Status"
+        color="#2a4af5"
+      />
+      <SyncStatusCard
+        synced={123.524}
+        total={172.503}
+        title="Consensus Sync Status"
+        color="#ff6161"
+      />
     </XStack>
   )
 }
