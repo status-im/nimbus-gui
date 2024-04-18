c2b19ea8bfee016efd248c3fd7a8527347f93beb
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-18 19:48:12 +0300

    fix(paired-device-card): main container, alignment and spacing

--- src/pages/ValidatorOnboarding/ValidatorSetup/ConsensusClient/PairedDeviceCard.tsx
+++ src/pages/ValidatorOnboarding/ValidatorSetup/ConsensusClient/PairedDeviceCard.tsx
@@ -1,24 +1,19 @@
 import { XStack, YStack } from 'tamagui'
 import { InfoBadgeIcon } from '@status-im/icons'
 import { Avatar, Text } from '@status-im/components'
-import { useWindowSize } from '../../../../hooks/useWindowSize'
+
+import BorderBox from '../../../../components/General/BorderBox'
 
 const PairedDeviceCard = () => {
-  const windowSize = useWindowSize()
   return (
-    <XStack
-      space={'$7'}
+    <BorderBox
       style={{
-        padding: '6px 12px',
-        border: '1px solid #DCE0E5',
-        borderRadius: '10px',
-        marginBottom: '20px',
-        width: windowSize.width < 580 ? '100%' : 'auto',
+        borderRadius: '10.1px',
+        borderWidth: '0.5px',
+        flex: '1',
       }}
-      justifyContent="space-between"
-      alignItems={'center'}
     >
-      <XStack space={'$3'}>
+      <XStack space={'$2'} alignItems="center" justifyContent={'space-between'}>
         <Avatar
           backgroundColor="pink"
           type="icon"
@@ -33,9 +28,9 @@ const PairedDeviceCard = () => {
             Stake & Chips
           </Text>
         </YStack>
+        <InfoBadgeIcon size={20} color="#A1ABBD" cursor={'pointer'} />
       </XStack>
-      <InfoBadgeIcon size={20} color="#A1ABBD" cursor={'pointer'} />
-    </XStack>
+    </BorderBox>
   )
 }
 
