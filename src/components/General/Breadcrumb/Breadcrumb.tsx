b65bf26127ad904c040dca48a3fc3d15a4256a33
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-25 20:32:24 +0300

    feat(breadcrumb): use tamagui XStack

--- src/components/General/Breadcrumb/Breadcrumb.tsx
+++ src/components/General/Breadcrumb/Breadcrumb.tsx
@@ -2,29 +2,25 @@ import { Text } from '@status-im/components'
 import { ChevronRightIcon } from '@status-im/icons'
 import { XStack } from 'tamagui'
 
-import './Breadcrumb.css'
-
 type BreadcrumbBarProps = {
   breadcrumbList: string[]
 }
 
 const Breadcrumb = ({ breadcrumbList }: BreadcrumbBarProps) => {
   return (
-    <nav className="breadcrumb-bar-nav">
-      <ul className="breadcrumb-bar-ul">
-        {breadcrumbList.map((item, index) => {
-          const isNotLast = index !== breadcrumbList.length - 1
-          return (
-            <XStack space={'$2'} alignItems="center" key={index}>
-              <Text size={15} color={isNotLast ? '#647084' : ''}>
-                {item}
-              </Text>
-              {isNotLast && <ChevronRightIcon size={20} color="#647084" />}
-            </XStack>
-          )
-        })}
-      </ul>
-    </nav>
+    <XStack space={'$2'} alignItems="center">
+      {breadcrumbList.map((item, index) => {
+        const isNotLast = index !== breadcrumbList.length - 1
+        return (
+          <XStack space={'$2'} alignItems="center" key={index}>
+            <Text size={15} color={isNotLast ? '#647084' : ''}>
+              {item}
+            </Text>
+            {isNotLast && <ChevronRightIcon size={20} color="#647084" />}
+          </XStack>
+        )
+      })}
+    </XStack>
   )
 }
 
