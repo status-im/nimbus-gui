1419e35a830374ff9f9b6ccc9ff9c51046ed2365
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-25 20:00:15 +0300

    feat(breadcrumb-bar): add chevron after route

--- src/components/General/BreadcrumbBar/BreadcrumbBar.tsx
+++ src/components/General/BreadcrumbBar/BreadcrumbBar.tsx
@@ -1,3 +1,6 @@
+import { ChevronRightIcon } from '@status-im/icons'
+import { XStack } from 'tamagui'
+
 import './BreadcrumbBar.css'
 
 type BreadcrumbBarProps = {
@@ -9,9 +12,12 @@ const BreadcrumbBar = ({ breadcrumbList }: BreadcrumbBarProps) => {
     <nav className="breadcrumb-bar-nav">
       <ul className="breadcrumb-bar-ul">
         {breadcrumbList.map((item, index) => (
-          <li className="breadcrumb-bar-li" key={index}>
-            {item}
-          </li>
+          <XStack space={'$2'} alignItems="center" key={index}>
+            <li className="breadcrumb-bar-li">{item}</li>
+            {index !== breadcrumbList.length - 1 && (
+              <ChevronRightIcon size={20} color="#647084" />
+            )}
+          </XStack>
         ))}
       </ul>
     </nav>
