7d402a108f5feca4e74b796235b3fbc8aed6b7b8
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-04-29 21:18:53 +0300

    feat(manage-validator): add and style breadcrumb

--- src/pages/ManageValidator/ManageValidator.tsx
+++ src/pages/ManageValidator/ManageValidator.tsx
@@ -1,8 +1,9 @@
-import { XStack } from 'tamagui'
+import { Separator, XStack, YStack } from 'tamagui'
 import { Outlet } from 'react-router-dom'
 
 import LeftManage from './LeftManage/LeftManage'
 import SidebarsWrapper from '../../components/PageWrappers/SidebarsWrapper'
+import Breadcrumb from '../../components/General/Breadcrumb/Breadcrumb'
 
 const ManageValidator = () => {
   return (
@@ -13,18 +14,25 @@ const ManageValidator = () => {
       isNotBottomPadding={true}
       isNotLeftPadding={true}
     >
-      <XStack
-        width={'min-content'}
-        alignItems="flex-start"
-        style={{
-          flexGrow: 1,
-        }}
-      >
-        <LeftManage />
-        <div style={{ flexGrow: 1 }}>
-          <Outlet />
+      <YStack style={{ width: '100%' }}>
+        <div style={{ padding: '12px 0 12px 16px' }}>
+          <Breadcrumb />
         </div>
-      </XStack>
+        <Separator borderColor={'#F0F2F5'} />
+        <XStack
+          width={'min-content'}
+          alignItems="flex-start"
+          style={{
+            flexGrow: 1,
+            width: '100%',
+          }}
+        >
+          <LeftManage />
+          <div style={{ flexGrow: 1 }}>
+            <Outlet />
+          </div>
+        </XStack>
+      </YStack>
     </SidebarsWrapper>
   )
 }
