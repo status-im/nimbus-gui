88ca7906665e9c81ab3abd603f5fa4d4faae7423
RadoslavDimchev <RadoslavDimchev111@gmail.com>
2024-05-08 09:49:16 +0300

    fix(validator-management): reduce gaps

--- src/pages/ValidatorManagement/ValidatorManagement.tsx
+++ src/pages/ValidatorManagement/ValidatorManagement.tsx
@@ -12,23 +12,24 @@ const ValidatorManagement = () => {
   return (
     <SidebarsWrapper isNotBottomPadding={true}>
       <YStack
-        space="$4"
+        space="$3"
         style={{ width: '100%' }}
         className={'transparent-scrollbar'}
       >
         <ManagementHeader />
-        <XStack space="$4" style={{ width: '100%' }}>
+        <XStack space="$3" style={{ width: '100%' }}>
           <DeviceUptime
-            style={{ flex: '0 0 calc(50% - 12px)', minHeight: '100%' }}
+            style={{ flex: '0 0 calc(50% - 8px)', minHeight: '100%' }}
           />
-          <XStack space="$4" style={{ width: '100%', display: 'flex' }}>
-            <YStack space="$4" style={{ flex: '0 0 calc(33.3% - 12px)' }}>
+          <XStack space="$3" style={{ width: '100%', display: 'flex' }}>
+            <YStack space="$3" style={{ flex: '0 0 calc(33.3% - 8px)' }}>
               <ConsensusUptimeCard />
               <ExecutionUptime />
             </YStack>
-            <YStack space="$4" style={{ flex: '0 0 calc(16.6% - 12px)' }}>
               <BasicInfoCard title="Network Validators" value="498,156" />
               <BasicInfoCard title="Queue" value="145" />
+            <YStack space="$3" style={{ flex: '0 0 calc(16.6% - 8px)' }}>
+              <BasicInfoCard
             </YStack>
           </XStack>
         </XStack>
